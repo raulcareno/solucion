e4da9078b897151c1a86346a4f424809f1f65d47
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2009-09-15 02:34:23 +0000

    14 de septiembre 2009 s

--- trunk/solucion/src/java/bean/matriculasBean.java
+++ trunk/solucion/src/java/bean/matriculasBean.java
@@ -53,25 +53,37 @@ public class matriculasBean {
 
                 
              //obtengo una nueva clave para la cabecera
-                List  listCabecera = adm.queryNativo("SELECT IF(MAX(invcab_codigo) IS NULL,0,MAX(invcab_codigo)) +1 FROM contable.invcabecera ");
-                Long cabeCodigo = (Long) ((Vector) listCabecera.get(0)).get(0);
-                String fechaActual = ((new Date()).getYear()+1900) +"-"+((new Date()).getMonth()+1)+"-"+((new Date()).getDate());
-                adm.ejecutaSqlNativo("INSERT INTO contable.invcabecera(invcab_codigo,invcab_fecharegistro,invcab_emision,invmae_codigo) " +
-                        "VALUES ( '"+cabeCodigo+"', '"+fechaActual+"', '"+fechaActual+"','"+maeCodigo+"');");
+//                List  listCabecera = adm.queryNativo("SELECT IF(MAX(invcab_codigo) IS NULL,0,MAX(invcab_codigo)) +1 FROM contable.invcabecera ");
+//                Long cabeCodigo = (Long) ((Vector) listCabecera.get(0)).get(0);
+//                String fechaActual = ((new Date()).getYear()+1900) +"-"+((new Date()).getMonth()+1)+"-"+((new Date()).getDate());
+//                adm.ejecutaSqlNativo("INSERT INTO contable.invcabecera(invcab_codigo,invcab_fecharegistro,invcab_emision,invmae_codigo) " +
+//                        "VALUES ( '"+cabeCodigo+"', '"+fechaActual+"', '"+fechaActual+"','"+maeCodigo+"');");
 
-             //ACA EMPIEZO A ITERAR LOS PRODUCTOS ASIGNADOS A  LA MATRICULA 
-                List produ = adm.queryNativo("SELECT pro.invpro_codigo, pro.invpro_precio1, invpro_descripcion   " +
-                        "FROM contable.mesesproductos me, contable.invproductos pro  WHERE me.glbemp_codigo = '"+periodo.getEmpresa()+"'  AND mes = 13" +
-                        " AND pro.invpro_codigo = me.invpro_codigo ");
-                for (int i  = 0;  i  < produ.size();  i ++) {
-                     Vector object = (Vector) produ.get(i);
-                     //obtengo una nueva clave para la detalle en movimiento
-                        List  listMovi = adm.queryNativo("SELECT IF(MAX(invmov_codigo) IS NULL,0,MAX(invmov_codigo)) +1 FROM contable.invmovimientos ");
-                        Long moviCodigo = (Long) ((Vector) listMovi.get(0)).get(0);
-                        adm.ejecutaSqlNativo("INSERT INTO contable.invmovimientos(invmov_codigo,invmov_descripcion,invmov_cantidad,invmov_totalmov,invpro_codigo,invcab_codigo,invmov_numeroorden) " +
-                             "VALUES ( '"+moviCodigo+"','"+object.get(2)+"','1',"+object.get(1)+","+object.get(0)+",'"+cabeCodigo+"', '13' )");
-                     
-                }
+//                //SELECCIONO EL MES QUE ES LA MATRICULA
+//                List mesMatricula = adm.queryNativo("SELECT mes_codigo FROM contable.meses me " +
+//                        "WHERE me.mes_matricula = true and me.glbani_codigo = '"+periodo.getEmpresa()+"'  ");
+//                Integer mesCodigo=0;
+//                if(mesMatricula.size()>0){
+//                        mesCodigo =  (Integer) ((Vector) mesMatricula.get(0)).get(0);
+//                  }else{
+//
+//                    return;
+//                  }
+//                //ACA EMPIEZO A ITERAR LOS PRODUCTOS ASIGNADOS A  LA MATRICULA
+//                List produ = adm.queryNativo("SELECT pro.invpro_codigo, pro.invpro_precio1, invpro_descripcion   " +
+//                        "FROM contable.mesesproductos me, contable.invproductos pro  " +
+//                        "WHERE me.glbemp_codigo = '"+periodo.getEmpresa()+"'  " +
+//                        "AND mes = '"+mesCodigo+"'  AND mes_curso  = '"+matricula.getCurso().getCodigocur()+"' " +
+//                        " AND pro.invpro_codigo = me.invpro_codigo ");
+//                for (int i  = 0;  i  < produ.size();  i ++) {
+//                     Vector object = (Vector) produ.get(i);
+//                     //obtengo una nueva clave para la detalle en movimiento
+//                        List  listMovi = adm.queryNativo("SELECT IF(MAX(invmov_codigo) IS NULL,0,MAX(invmov_codigo)) +1 FROM contable.invmovimientos ");
+//                        Long moviCodigo = (Long) ((Vector) listMovi.get(0)).get(0);
+//                        adm.ejecutaSqlNativo("INSERT INTO contable.invmovimientos(invmov_codigo,invmov_descripcion,invmov_cantidad,invmov_totalmov,invpro_codigo,invcab_codigo,invmov_numeroorden) " +
+//                             "VALUES ( '"+moviCodigo+"','"+object.get(2)+"','1',"+object.get(1)+","+object.get(0)+",'"+cabeCodigo+"', '13' )");
+//
+//                }
                 
             }
         }
