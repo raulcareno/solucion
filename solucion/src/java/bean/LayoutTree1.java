0c758c51f586fe54c5a1d3aa5e57e61a7f485e6a
jcinform <jcinform@7aa694e0-94bd-11de-8746-af773c80d763>
2010-01-29 21:49:49 +0000

    25 enero 2010

---
+++ solucion/src/java/bean/LayoutTree1.java
@@ -0,0 +1,73 @@
+package bean;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.zkoss.zk.ui.*;
+import org.zkoss.zk.ui.ext.AfterCompose;
+import org.zkoss.zul.*;
+
+/**
+ * The Table-of-Content tree on the left.
+ *
+ * @author jumperchen
+ */
+public class LayoutTree1 extends Tree implements AfterCompose {
+	public LayoutTree1() {
+	}
+ 
+	public void onSelect() {
+         Treeitem item = getSelectedItem();
+          
+         String menu = item.getId();
+         if(menu.contains("ss")){
+             if(item != null) {
+                    Include inc = (Include)getSpaceOwner().getFellow("xcontents");
+                    inc.setSrc("/blanco.zul");
+                   
+                    
+                }
+             return;
+         }
+         Permisos per = new Permisos();
+         try{
+         if(per.verificarPermiso(menu, "Ingresar")){
+                if (item != null) {
+                    Include inc = (Include)getSpaceOwner().getFellow("xcontents");
+                    inc.setSrc((String)item.getValue());
+                    
+                    Label la = (Label) getSpaceOwner().getFellow("propiedad");
+                    la.setValue("El Menú Seleccionado actualmente es: "+ menu);
+                }
+                 
+         }else{
+                if(item != null) {
+                    Include inc = (Include)getSpaceOwner().getFellow("xcontents");
+                    inc.setSrc("/denegado.zul");
+                }
+         }
+         }catch(Exception e){
+                    Include inc = (Include)getSpaceOwner().getFellow("xcontents");
+                    inc.setSrc("/denegado.zul");
+         }
+		
+	}
+	public void afterCompose() {
+		final Execution exec = Executions.getCurrent();
+		String id = exec.getParameter("id");
+
+		Treeitem item = null;
+		if (id != null) {
+			try {
+				item = (Treeitem)getSpaceOwner().getFellow(id);
+			} catch (ComponentNotFoundException ex) { //ignore
+			}
+		}
+
+		if (item == null)
+			item = (Treeitem)getSpaceOwner().getFellow("ss1");
+		exec.setAttribute("contentSrc", (String)item.getValue());
+
+			//so index.zul know which page to load based on the id parameter
+		selectItem(item);
+	}
+}
