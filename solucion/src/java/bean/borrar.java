fa56bda4d1f5f2c98e65cfd4ddeaa0c4d8cdff65
jcinform <jcinform@gmail.com>
2010-04-21 20:36:06 +0000

    6 de abril modificado subversion

--- solucion/src/java/bean/borrar.java
+++ solucion/src/java/bean/borrar.java
@@ -2,21 +2,29 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package bean;
 
+import com.linuxense.javadbf.DBFException;
+import com.linuxense.javadbf.DBFField;
+import com.linuxense.javadbf.DBFReader;
+import com.linuxense.javadbf.DBFWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jcinform.persistencia.*;
 import jcinform.procesos.Administrador;
 import org.zkoss.image.AImage;
 
- 
+
 import org.zkoss.util.media.Media;
 import org.zkoss.zul.Fileupload;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Listcell;
-
 
 /**
  *t
@@ -24,15 +32,16 @@ import org.zkoss.zul.Listcell;
  */
 public class borrar {
 //Panel p;
-void func() throws InterruptedException{
-    Media media = Fileupload.get();
-    Listcell c = null;
-    AImage a= null;
+
+    void func() throws InterruptedException {
+        Media media = Fileupload.get();
+        Listcell c = null;
+        AImage a = null;
 
 //    c.setImageContent(a);
-    
-    
-    //c.setImageContent(null);
+
+
+        //c.setImageContent(null);
 
 //    Listbox l;
 //    l.getItems().get(0);
@@ -42,25 +51,24 @@ void func() throws InterruptedException{
 //    ite.getValue()
 
 
- 
+
 
 //    media.getByteData();
 //    AImage a;
 //    a.getByteData()
 //    Combobox c;
 //    c.getSelectedItem().getValue()
-     
+
 //			byte[] archivo=(((AMedia) media).getStringData());
 //    FileOutputStream a;
 //    String ads;
 //    ads.getBytes()
 //
-    
 
-}
- 
 
-        public static void bandejaEstudiante(Estudiantes est){
+    }
+
+    public static void bandejaEstudiante(Estudiantes est) {
 
 //            Tablechildren sds;
 //                    sds.se;
@@ -68,8 +76,8 @@ void func() throws InterruptedException{
 //
 //        List<Correos> a = adm.query("Select o from Correos as o where o.destinatario = '"+est.getCodigoest()+"' " +
 //        "and  o.tiporemitente = 'P' and o.eliminado = false and o.archivado = false ");
-            //System.out.println(""+a.get(0).getTema() + " "+ a.get(0).getRemitenten());
-         
+        //System.out.println(""+a.get(0).getTema() + " "+ a.get(0).getRemitenten());
+
         Listbox b;
 //        b.getSelectedIndex();
 //b.getRows()
@@ -85,25 +93,42 @@ void func() throws InterruptedException{
 //							}
 
     }
-static claves cla = new claves();
+    static claves cla = new claves();
+    static String ubicacion2 = new String("/home/geovanny/Escritorio/base.dbf");
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            Administrador adm = new Administrador();
+            List<Matriculas> estu = adm.query("Select o from Matriculas as o where o.codigomat > 0  ");
 
-        Administrador adm = new Administrador();
-        List<Representante> estu = adm.query("Select o from Representante as o");
-        for (Iterator<Representante> it = estu.iterator(); it.hasNext();) {
-            Representante estudiantes = it.next();
-            estudiantes.setClave(encriptar(estudiantes.getUsuario()));
-            adm.actualizar(estudiantes);
+            crearDBF(estu);
 
+//            crearDBF(estu.get(0));
+            Object[] ab = leerPdf(ubicacion2, new Double(5));
+            System.out.println("" + ab[0] + " " + ab[3]);
+
+            //        Administrador adm = new Administrador();
+            //        List<Representante> estu = adm.query("Select o from Representante as o");
+            //        for (Iterator<Representante> it = estu.iterator(); it.hasNext();) {
+            //            Representante estudiantes = it.next();
+            //            estudiantes.setClave(encriptar(estudiantes.getUsuario()));
+            //            adm.actualizar(estudiantes);
+            //        }
+            //        }
+        } catch (DBFException ex) {
+            Logger.getLogger(borrar.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(borrar.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-   
+
+
     }
-       public static String encriptar(String clave) {
+
+    public static String encriptar(String clave) {
         try {
 
             if (clave.equals(null) || clave.equals("")) {
@@ -117,4 +142,158 @@ static claves cla = new claves();
         }
     }
 
+    public static Object[] leerPdf(String ubicacion, Double codigo) {
+        try {
+            System.out.println("Iniciamos proceso");
+            InputStream inputStream = new FileInputStream(ubicacion);
+//            InputStream inputStream = new FileInputStream(new String("/home/geovanny/Escritorio/interfase.dbf"));
+            DBFReader reader = new DBFReader(inputStream);
+            Object[] rowObjects;
+            while ((rowObjects = reader.nextRecord()) != null) {
+                if (codigo.equals(rowObjects[0])) {
+                    inputStream.close();
+                    return rowObjects;
+                }
+//                System.out.print(rowObjects[0].toString() + " ");
+//                System.out.println(((String) rowObjects[1]).trim());
+
+
+            }
+            inputStream.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(matriculasBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+
+    }
+
+    public static DBFField[] estructura() {
+        DBFField fields[] = new DBFField[17];
+        fields[0] = new DBFField();
+        fields[0].setName("CODIGO");
+        fields[0].setFieldName("CODIGO");
+        fields[0].setDataType(DBFField.FIELD_TYPE_N);
+        fields[0].setFieldLength(10);
+
+        fields[1] = new DBFField();
+        fields[1].setFieldName("CODIGOM");
+        fields[1].setDataType(DBFField.FIELD_TYPE_C);
+        fields[1].setFieldLength(100);
+
+
+        fields[2] = new DBFField();
+        fields[2].setFieldName("CEDULA");
+        fields[2].setDataType(DBFField.FIELD_TYPE_C);
+        fields[2].setFieldLength(100);
+
+
+        fields[3] = new DBFField();
+        fields[3].setFieldName("APELLIDOS");
+        fields[3].setDataType(DBFField.FIELD_TYPE_C);
+        fields[3].setFieldLength(100);
+
+        fields[4] = new DBFField();
+        fields[4].setFieldName("NOMBRES");
+        fields[4].setDataType(DBFField.FIELD_TYPE_C);
+        fields[4].setFieldLength(100);
+
+        fields[5] = new DBFField();
+        fields[5].setFieldName("SEXO");
+        fields[5].setDataType(DBFField.FIELD_TYPE_C);
+        fields[5].setFieldLength(100);
+
+        fields[6] = new DBFField();
+        fields[6].setFieldName("DIRECCION");
+        fields[6].setDataType(DBFField.FIELD_TYPE_C);
+        fields[6].setFieldLength(100);
+
+        fields[7] = new DBFField();
+        fields[7].setFieldName("TELEFONO");
+        fields[7].setDataType(DBFField.FIELD_TYPE_C);
+        fields[7].setFieldLength(100);
+
+        fields[8] = new DBFField();
+        fields[8].setFieldName("CELULAR");
+        fields[8].setDataType(DBFField.FIELD_TYPE_C);
+        fields[8].setFieldLength(100);
+
+        fields[9] = new DBFField();
+        fields[9].setFieldName("FECHANAC");
+        fields[9].setDataType(DBFField.FIELD_TYPE_D);
+
+
+        fields[10] = new DBFField();
+        fields[10].setFieldName("INSANT");
+        fields[10].setDataType(DBFField.FIELD_TYPE_C);
+        fields[10].setFieldLength(100);
+
+        fields[11] = new DBFField();
+        fields[11].setFieldName("NIVEL");
+        fields[11].setDataType(DBFField.FIELD_TYPE_C);
+        fields[11].setFieldLength(100);
+
+        fields[12] = new DBFField();
+        fields[12].setFieldName("FECHAING");
+        fields[12].setDataType(DBFField.FIELD_TYPE_D);
+//        fields[12].setFieldLength(100);
+
+        fields[13] = new DBFField();
+        fields[13].setFieldName("NOMFAC");
+        fields[13].setDataType(DBFField.FIELD_TYPE_C);
+        fields[13].setFieldLength(100);
+
+        fields[14] = new DBFField();
+        fields[14].setFieldName("DIRECFAC");
+        fields[14].setDataType(DBFField.FIELD_TYPE_C);
+        fields[14].setFieldLength(100);
+
+        fields[15] = new DBFField();
+        fields[15].setFieldName("RUCFAC");
+        fields[15].setDataType(DBFField.FIELD_TYPE_C);
+        fields[15].setFieldLength(100);
+
+        fields[16] = new DBFField();
+        fields[16].setFieldName("TELFAC");
+        fields[16].setDataType(DBFField.FIELD_TYPE_C);
+        fields[16].setFieldLength(100);
+
+        return fields;
+    }
+
+    public static void crearDBF(List<Matriculas> matriculas) throws DBFException, IOException {
+        DBFField fields[] = estructura();
+        DBFWriter writer = new DBFWriter();
+        writer.setFields(fields);
+
+// now populate DBFWriter
+//for
+        for (Iterator<Matriculas> it = matriculas.iterator(); it.hasNext();) {
+            Matriculas matricula = it.next();
+
+            Object rowData[] = new Object[17];
+            rowData[0] = new Double(matricula.getEstudiante().getCodigoest());
+            rowData[1] = matricula.getCodigomat() + "";
+            rowData[2] = matricula.getEstudiante().getCedula();
+            rowData[3] = matricula.getEstudiante().getApellido();
+            rowData[4] = matricula.getEstudiante().getNombre();
+            rowData[5] = matricula.getEstudiante().getGenero();
+            rowData[6] = matricula.getEstudiante().getDireccion();
+            rowData[7] = matricula.getEstudiante().getTelefono();
+            rowData[8] = matricula.getEstudiante().getTelefono();
+            rowData[9] = matricula.getEstudiante().getFechanacimiento();
+            rowData[10] = matricula.getInstitucion();
+            rowData[11] = matricula.getCurso() + "";
+            rowData[12] = matricula.getFechains();
+            rowData[13] = matricula.getEstudiante().getRepresentante().getNombrefactura();
+            rowData[14] = matricula.getEstudiante().getRepresentante().getDirfactura();
+            rowData[15] = matricula.getEstudiante().getRepresentante().getIdentificacionfactura();
+            rowData[16] = matricula.getEstudiante().getRepresentante().getTelfactura();
+
+            writer.addRecord(rowData);
+        }
+        FileOutputStream fos = new FileOutputStream("/home/geovanny/Escritorio/base.dbf");
+        writer.write(fos);
+        fos.close();
+    }
 }
--- solucion/src/java/bean/matriculasBean.java
+++ solucion/src/java/bean/matriculasBean.java
@@ -4,9 +4,15 @@
  */
 package bean;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Vector;
+import com.linuxense.javadbf.DBFException;
+import com.linuxense.javadbf.DBFField;
+import com.linuxense.javadbf.DBFReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jcinform.persistencia.Matriculas;
 import jcinform.persistencia.Periodo;
 import jcinform.procesos.Administrador;
@@ -23,69 +29,62 @@ public class matriculasBean {
     }
 
     public void generar(Boolean repreNuevo, Boolean estuNuevo, Matriculas matricula, Periodo periodo) {
-        List maestroNuevo = adm.queryNativo("SELECT IF(MAX(invmae_codigo) IS NULL,0,MAX(invmae_codigo)) +1 FROM contable.invmaestro");
-        Long maeCodigo = (Long) ((Vector) maestroNuevo.get(0)).get(0);
-        Long codigoRepresentante = null;
-        if (repreNuevo){
-            adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_telefono1,invmae_tipoid,invmae_nombre,invmae_numeroruc,invmae_direccion,glbemp_codigo,invmae_codigo,invmae_grupo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getRepresentante().getCodigorep() + ",'" + matricula.getEstudiante().getRepresentante().getTelfactura() + "','" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "','" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "','" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "','" + matricula.getEstudiante().getRepresentante().getDirfactura() + "'," + periodo.getCodigoper() + "," + maeCodigo + ", 'REP', 1 )");
-            codigoRepresentante = maeCodigo;
-        } else {
-            List datos = adm.queryNativo("select invmae_codigo from contable.invmaestro where invmae_diascredito = " + matricula.getEstudiante().getRepresentante().getCodigorep() + " and invmae_grupo = 'REP' ");
-            System.out.println("ENCONTRADOS " + datos);
-            if (datos.size() > 0) {
-                codigoRepresentante = new Long(((Vector) datos.get(0)).get(0).toString());
-                adm.ejecutaSqlNativo("update contable.invmaestro set invmae_telefono1='" + matricula.getEstudiante().getRepresentante().getTelfactura() + "',invmae_tipoid='" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "',invmae_nombre='" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "', invmae_numeroruc='" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "',   invmae_direccion='" + matricula.getEstudiante().getRepresentante().getDirfactura() + "' , invmae_representante = '" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', invmae_grupo = 'REP', glbemp_codigo = " + periodo.getCodigoper() + ", invmae_activo = 1  where invmae_codigo = " + codigoRepresentante + " ");
-            } else {
-                adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_telefono1,invmae_tipoid,invmae_nombre,invmae_numeroruc,invmae_direccion,glbemp_codigo,invmae_codigo,invmae_grupo,invmae_activo) values ( " + matricula.getEstudiante().getRepresentante().getCodigorep() + ",'" + matricula.getEstudiante().getRepresentante().getTelfactura() + "','" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "','" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "','" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "','" + matricula.getEstudiante().getRepresentante().getDirfactura() + "'," + periodo.getCodigoper() + ",'" + maeCodigo + "', 'REP',1 )");
-                codigoRepresentante = maeCodigo;
-            }
-        }
-        maestroNuevo = adm.queryNativo("SELECT IF(MAX(invmae_codigo) IS NULL,0,MAX(invmae_codigo)) +1 FROM contable.invmaestro");
-        maeCodigo = (Long) ((Vector) maestroNuevo.get(0)).get(0);
-        if (estuNuevo) {
-            adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_nombre,invmae_grupo,invmae_codigo,inv_invmae_codigo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getCodigoest() + ",'" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', 'EST', " + maeCodigo + ", " + codigoRepresentante + "," + periodo.getCodigoper() + ", 1 )");
-        } else {
-            List datos = adm.queryNativo("select invmae_codigo from contable.invmaestro where invmae_diascredito = " + matricula.getEstudiante().getCodigoest() + "  and invmae_grupo = 'EST' ");
-            if (datos.size() > 0) {
-                adm.ejecutaSqlNativo("update contable.invmaestro set invmae_nombre = '" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', glbemp_codigo = " + periodo.getCodigoper() + ", invmae_activo = 1  where invmae_diascredito= " + matricula.getEstudiante().getCodigoest() + " and invmae_grupo = 'EST' ");
-
-            }else{
-                adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_nombre,invmae_grupo,invmae_codigo,inv_invmae_codigo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getCodigoest() + ",'" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "','EST', " + maeCodigo + ", " + codigoRepresentante + "," + periodo.getCodigoper() + ",1 )");
-             //obtengo una nueva clave para la cabecera
-//                List  listCabecera = adm.queryNativo("SELECT IF(MAX(invcab_codigo) IS NULL,0,MAX(invcab_codigo)) +1 FROM contable.invcabecera ");
-//                Long cabeCodigo = (Long) ((Vector) listCabecera.get(0)).get(0);
-//                String fechaActual = ((new Date()).getYear()+1900) +"-"+((new Date()).getMonth()+1)+"-"+((new Date()).getDate());
-//                adm.ejecutaSqlNativo("INSERT INTO contable.invcabecera(invcab_codigo,invcab_fecharegistro,invcab_emision,invmae_codigo) " +
-//                        "VALUES ( '"+cabeCodigo+"', '"+fechaActual+"', '"+fechaActual+"','"+maeCodigo+"');");
-
-//                //SELECCIONO EL MES QUE ES LA MATRICULA
-//                List mesMatricula = adm.queryNativo("SELECT mes_codigo FROM contable.meses me " +
-//                        "WHERE me.mes_matricula = true and me.glbani_codigo = '"+periodo.getEmpresa()+"'  ");
-//                Integer mesCodigo=0;
-//                if(mesMatricula.size()>0){
-//                        mesCodigo =  (Integer) ((Vector) mesMatricula.get(0)).get(0);
-//                  }else{
+//        if (repreNuevo) {
+//            adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_telefono1,invmae_tipoid,invmae_nombre,invmae_numeroruc,invmae_direccion,glbemp_codigo,invmae_codigo,invmae_grupo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getRepresentante().getCodigorep() + ",'" + matricula.getEstudiante().getRepresentante().getTelfactura() + "','" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "','" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "','" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "','" + matricula.getEstudiante().getRepresentante().getDirfactura() + "'," + periodo.getCodigoper() + "," + maeCodigo + ", 'REP', 1 )");
 //
-//                    return;
-//                  }
-//                //ACA EMPIEZO A ITERAR LOS PRODUCTOS ASIGNADOS A  LA MATRICULA
-//                List produ = adm.queryNativo("SELECT pro.invpro_codigo, pro.invpro_precio1, invpro_descripcion   " +
-//                        "FROM contable.mesesproductos me, contable.invproductos pro  " +
-//                        "WHERE me.glbemp_codigo = '"+periodo.getEmpresa()+"'  " +
-//                        "AND mes = '"+mesCodigo+"'  AND mes_curso  = '"+matricula.getCurso().getCodigocur()+"' " +
-//                        " AND pro.invpro_codigo = me.invpro_codigo ");
-//                for (int i  = 0;  i  < produ.size();  i ++) {
-//                     Vector object = (Vector) produ.get(i);
-//                     //obtengo una nueva clave para la detalle en movimiento
-//                        List  listMovi = adm.queryNativo("SELECT IF(MAX(invmov_codigo) IS NULL,0,MAX(invmov_codigo)) +1 FROM contable.invmovimientos ");
-//                        Long moviCodigo = (Long) ((Vector) listMovi.get(0)).get(0);
-//                        adm.ejecutaSqlNativo("INSERT INTO contable.invmovimientos(invmov_codigo,invmov_descripcion,invmov_cantidad,invmov_totalmov,invpro_codigo,invcab_codigo,invmov_numeroorden) " +
-//                             "VALUES ( '"+moviCodigo+"','"+object.get(2)+"','1',"+object.get(1)+","+object.get(0)+",'"+cabeCodigo+"', '13' )");
+//        } else {
+//            List datos = adm.queryNativo("select invmae_codigo from contable.invmaestro where invmae_diascredito = " + matricula.getEstudiante().getRepresentante().getCodigorep() + " and invmae_grupo = 'REP' ");
+//            System.out.println("ENCONTRADOS " + datos);
+//            if (datos.size() > 0) {
+//                codigoRepresentante = new Long(((Vector) datos.get(0)).get(0).toString());
+//                adm.ejecutaSqlNativo("update contable.invmaestro set invmae_telefono1='" + matricula.getEstudiante().getRepresentante().getTelfactura() + "',invmae_tipoid='" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "',invmae_nombre='" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "', invmae_numeroruc='" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "',   invmae_direccion='" + matricula.getEstudiante().getRepresentante().getDirfactura() + "' , invmae_representante = '" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', invmae_grupo = 'REP', glbemp_codigo = " + periodo.getCodigoper() + ", invmae_activo = 1  where invmae_codigo = " + codigoRepresentante + " ");
+//            } else {
+//                adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_telefono1,invmae_tipoid,invmae_nombre,invmae_numeroruc,invmae_direccion,glbemp_codigo,invmae_codigo,invmae_grupo,invmae_activo) values ( " + matricula.getEstudiante().getRepresentante().getCodigorep() + ",'" + matricula.getEstudiante().getRepresentante().getTelfactura() + "','" + matricula.getEstudiante().getRepresentante().getTipoidentificacion() + "','" + matricula.getEstudiante().getRepresentante().getNombrefactura() + "','" + matricula.getEstudiante().getRepresentante().getIdentificacionfactura() + "','" + matricula.getEstudiante().getRepresentante().getDirfactura() + "'," + periodo.getCodigoper() + ",'" + maeCodigo + "', 'REP',1 )");
+//                codigoRepresentante = maeCodigo;
+//            }
+//        }
+//        maestroNuevo = adm.queryNativo("SELECT IF(MAX(invmae_codigo) IS NULL,0,MAX(invmae_codigo)) +1 FROM contable.invmaestro");
+//        maeCodigo = (Long) ((Vector) maestroNuevo.get(0)).get(0);
+//        if (estuNuevo) {
+//            adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_nombre,invmae_grupo,invmae_codigo,inv_invmae_codigo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getCodigoest() + ",'" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', 'EST', " + maeCodigo + ", " + codigoRepresentante + "," + periodo.getCodigoper() + ", 1 )");
+//        } else {
+//            List datos = adm.queryNativo("select invmae_codigo from contable.invmaestro where invmae_diascredito = " + matricula.getEstudiante().getCodigoest() + "  and invmae_grupo = 'EST' ");
+//            if (datos.size() > 0) {
+//                adm.ejecutaSqlNativo("update contable.invmaestro set invmae_nombre = '" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "', glbemp_codigo = " + periodo.getCodigoper() + ", invmae_activo = 1  where invmae_diascredito= " + matricula.getEstudiante().getCodigoest() + " and invmae_grupo = 'EST' ");
 //
-//                }
+//            } else {
+//                adm.ejecutaSqlNativo("insert into contable.invmaestro (invmae_diascredito,invmae_nombre,invmae_grupo,invmae_codigo,inv_invmae_codigo,glbemp_codigo,invmae_activo) values ( " + matricula.getEstudiante().getCodigoest() + ",'" + matricula.getEstudiante().getApellido() + " " + matricula.getEstudiante().getNombre() + "','EST', " + maeCodigo + ", " + codigoRepresentante + "," + periodo.getCodigoper() + ",1 )");
+//
+//
+//            }
+//        }
+    }
+
+    public void leerPdf() {
+        try {
+            System.out.println("Iniciamos proceso");
+            InputStream inputStream = new FileInputStream(new String("/home/geovanny/Escritorio/interfase.dbf"));
+            DBFReader reader = new DBFReader(inputStream);
+            // get the field count if you want for some reasons like the following
+//            int numberOfFields = reader.getFieldCount();
+//            for (int i = 0; i < numberOfFields; i++) {
+//                DBFField field = reader.getField(i);
+//                System.out.print(i+"::"+field.getName()+"   ");
+//            }
+            System.out.println("*********************************************");
+            Object[] rowObjects;
+            while ((rowObjects = reader.nextRecord()) != null) {
+                System.out.println("*********************************************");
+                System.out.print(((String)rowObjects[0]).trim());
+//                System.out.print(((String)rowObjects[3]).trim()+" "+((String)rowObjects[4]).trim()+"");
+                
                 
             }
+            inputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(matriculasBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         }
 
     }
-}
