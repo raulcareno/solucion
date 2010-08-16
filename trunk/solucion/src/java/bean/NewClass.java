3644dca83223503ab2bd9de11263a750d7b75143
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2010-08-16 04:29:38 +0000

    playa

--- trunk/solucion/src/java/bean/NewClass.java
+++ trunk/solucion/src/java/bean/NewClass.java
@@ -1,37 +1,45 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<?link rel="shortcut icon" type="image/icono" href="images/icono.png"?>
-<zk xmlns="http://www.zkoss.org/2005/zul">
- 
-<window id="impresion" onCreate="notasd()" height="100%" border="normal">
- <jasperreport visible="true" id="report"  height="90%"  />
-       <button id="bimprimir"  visible="false" label="CLICK AQUI SI NO  LOGRO VISUALIZAR EL COMPROBANTE DE MATRICULA" onClick="pdf()" />
-  
- <zscript><![CDATA[
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import jcinform.persistencia.*;
+package bean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import jcinform.persistencia.Institucion;
+import jcinform.persistencia.Matriculas;
+import jcinform.persistencia.Periodo;
 import jcinform.procesos.Administrador;
 import net.sf.jasperreports.engine.JRDataSource;
-import net.sf.jasperreports.engine.JRDataSource;
-import sources.*;
-import java.util.ArrayList;
-//PARA REPORTES
-String codigo = "";
-pdf(){
+import org.zkoss.zk.ui.Executions;
+import sources.ReporteActaDataSource;
+
+/**
+ *
+ * @author Geovanny Jadan
+ */
+public class NewClass {
+
+
+    String codigo = "";
+public void pdf(){
         Executions.sendRedirect("/592981728937491235M.zul?AASL2KSO348934934="+ codigo +"&TP=html");
-        
+
 }
- 
 
 public ArrayList certificados(Matriculas matricula,String tipo){
       Administrador adm = new Administrador();
        Periodo per  = new Periodo();
         ArrayList detalle = new ArrayList();
-        ArrayList arr = new ArrayList();
         String query = "SELECT mat FROM Matriculas AS mat " +
                 "WHERE  mat.estado like '%Matriculado%' " +
                 "and mat.codigomat = '" + matricula.getCodigomat() + "' ";
         List hoy = adm.query(query);
-        
+
         for (Iterator it = hoy.iterator(); it.hasNext();) {
             Matriculas elem = (Matriculas) it.next();
             detalle.add(elem);
@@ -45,39 +53,35 @@ public ArrayList certificados(Matriculas matricula,String tipo){
             parametros.put("slogan", insts.getSlogan());
             parametros.put("secretaria", insts.getSecretaria());
             parametros.put("rectora", insts.getRector());
-JRDataSource ds = new ReporteActaDataSource(detalle);
-            
-            arr.add(ds);
+            ArrayList arr = new ArrayList();
+            arr.add(new ReporteActaDataSource(detalle));
             arr.add(parametros);
          return  arr;
 }
 void notasd(){
-
+             Administrador adm = new Administrador();
             Integer valor =  new Integer(Executions.getCurrent().getParameter("AASL2KSO348934934"));
             codigo = valor+"";
             String tipo =  Executions.getCurrent().getParameter("TP");
-             
+
+            JRDataSource datasource = null;
             ArrayList arreglo = certificados(new Matriculas(valor),"AM");
                     datasource = (ReporteActaDataSource) arreglo.get(0);
                     Map parametros = (HashMap) arreglo.get(1);
-                     
+
                     parametros.put("titulo", "Comprobante de Matricula");
-                    report.setSrc("WEB-INF/reportes/actaMatricula2.jasper");
-                    report.setParameters(parametros);
-                    report.setDatasource(datasource);
-                    report.setType(tipo);
-              bimprimir.visible=true;
+//                    report.setSrc("WEB-INF/reportes/actaMatricula2.jasper");
+//                    report.setParameters(parametros);
+//                    report.setDatasource(datasource);
+//                    report.setType(tipo);
+//              bimprimir.visible=true;
              if(tipo.contains("html")){
-                    bimprimir.visible = false;
-                    report.setHeight("100%");
+//                    bimprimir.visible = false;
+//                    report.setHeight("100%");
              }
 
 
             }
 
- ]]>
-</zscript>
-          
-</window>
 
-</zk>
+}
