ce194eb4ab92a7bd2985f22975f27fa1e00eff97
jcinform <jcinform@gmail.com>
2010-09-08 13:45:17 +0000

    23-08-2010

---
+++ solucion/src/java/bean/pruebas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bean;
+
+/**
+ *
+ * @author Ismael Jadan
+ */
+public class pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Double d = new Double("00");
+        System.out.println(""+d);
+    }
+
+}
