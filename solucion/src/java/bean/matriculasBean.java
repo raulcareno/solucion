1e9498193d3586cfb5a4ba67798c46e88e18f5a3
jcinform <jcinform@gmail.com>
2011-01-06 16:18:31 +0000

    05012011

--- solucion/src/java/bean/matriculasBean.java
+++ solucion/src/java/bean/matriculasBean.java
@@ -1,6 +1,11 @@
 package bean;
-
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.List;
+import jcinform.persistencia.Estudiantes;
 import java.io.FileWriter;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import jcinform.persistencia.Matriculas;
 import jcinform.persistencia.Periodo;
@@ -22,21 +27,72 @@ public class matriculasBean {
 //            Logger.getLogger(matriculasBean.class.getName()).log(Level.SEVERE, null, ex);
 //        }
     }
-
+//     public static void export_to_csv(Listbox listbox) {
+        //exp.exportar(listbox);
+        //return;
+      
+//        Filedownload.save(sb.toString().getBytes(), "application/vnd.ms-excel", "auditoria.csv");
+//        }
     public void generar() {
         FileWriter fichero = null;
         PrintWriter pw = null;
         try {
-            fichero = new FileWriter("/home/usuario/base/datos.txt");
+            String nombre = "/home/usuario/base/datos.txt";
+              nombre = "F:/datos.csv";
+            fichero = new FileWriter(nombre);
+            
             pw = new PrintWriter(fichero);
-                adm.queryNativo("SELECT est.*,mat.codigomat,cur.descripcion "
+              List listado = adm.queryNativo("SELECT est.*,mat.codigomat,cur.descripcion "
                         + "FROM estudiantes est, matriculas mat, cursos cur "
                         + "WHERE mat.estudiante = est.codigoest  "
-                        + "and mat.curso = cur.codigocur "
-                    + "    INTO OUTFILE '/home/usuario/base/datos.txt' "
-                    + "    FIELDS TERMINATED BY ';' "
-                    + "    OPTIONALLY ENCLOSED BY '\"' "
-                    + "    LINES TERMINATED BY '\\n\\r'; ");
+                        + "and mat.curso = cur.codigocur ");
+            
+            
+              
+                 for (Iterator it = listado.iterator(); it.hasNext();) {
+                        Object object = it.next(); 
+                  }
+                
+                  String s = ";\t";
+        StringBuffer sb = new StringBuffer();
+        //Field[] campos = Estudiantes.class.getDeclaredFields();
+        List campos = adm.queryNativo("desc estudiantes");
+        String h = "";
+            for (Iterator it = campos.iterator(); it.hasNext();) {
+                Field[] object = (Field[]) it.next();
+                         Field field = object[0];
+                  h+=field.getName()+s;
+                  System.out.print(""+h);
+              }  
+            System.out.println(""+h);
+                    sb.append(h+"\n");
+//            for (int i = 0; i < campos.length; i++) {
+//                String h = "";
+//                Field field = campos[i];
+//                    h+=field.getName()+s;
+//                    System.out.println(""+h);
+//                    sb.append(h+"\n");
+//            }
+        
+//        for (Object head : listbox.getHeads()) {
+//            String h = "";
+//            for (Object header : ((Listhead) head).getChildren()) {
+//                h += ((Listheader) header).getLabel() + s;
+//            }
+//            sb.append(h + "\n");
+//        }
+//            for (Object item : listbox.getItems()) {
+//                String i = "";
+//                for (Object cell : ((Listitem) item).getChildren()) {
+//                    i += ((Listcell) cell).getLabel() + s;
+//                }
+//                sb.append(i + "\n");
+//            }
+            OutputStream out = new FileOutputStream(nombre);
+            out.write(sb.toString().getBytes());
+            out.close();
+            
+
             //List<Matriculas> estu = adm.query("Select o from Matriculas as o where o.estado = 'Matriculado' ");
 //            for (Iterator<Matriculas> it = estu.iterator(); it.hasNext();) {
 //                Matriculas matricula = it.next();
