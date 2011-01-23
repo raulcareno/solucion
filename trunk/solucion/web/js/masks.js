64e183a7ba6a2e14337e25d260ca317f129c81b3
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2011-01-23 05:10:11 +0000

    13012011

--- trunk/solucion/web/js/masks.js
+++ trunk/solucion/web/js/masks.js
@@ -1,17 +1,22 @@
-      function applyZipCodeMask(compId){
-        applyMask(compId, "99.999-999")
-      }
+function applyCelular(compId){
+   applyMask(compId, "999-999-999")
+}
 
-      function applyPhoneMask(compId){
-          applyMask(compId, "999-999-999");
-      }
+function applyTelefono(compId){
+   // alert("HOLA");
+   applyMask(compId, "9999-999");
+}
 
-      function applyMask(compId, mask){
-          compId = '#' + compId;
-         jQuery(compId).mask(mask);
-      }
-      function aparecer(compId){
-         compId = '#' + compId;
-         jQuery(compId).hide().fadeIn();
+function applyMask(compId, mask){
+   compId = '#' + compId;
+   jQuery(compId).mask(mask);
+ // jQuery(compId).Watermark(compId,"#369");
+   //$("#suffix").Watermark("Suffix","#369");
+}
+function animar(compId){
+   compId = '#' + compId;
 
-      }
+   jQuery(compId).animar();
+    
+   //$("#suffix").Watermark("Suffix","#369");
+}
