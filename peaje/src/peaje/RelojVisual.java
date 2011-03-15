4320b30bd758839f3e10b456c328bc440d38feef
jcinform <jcinform@gmail.com>
2011-03-15 00:22:42 +0000

    13012011

--- peaje/src/peaje/RelojVisual.java
+++ peaje/src/peaje/RelojVisual.java
@@ -40,7 +40,8 @@ public class RelojVisual extends JLabel
 
                           setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 40)); // NOI18N
                           //setBounds(190, 100, 210, 500);
-                        setForeground(new java.awt.Color(255, 153, 0));
+                          //#003366
+                        setForeground(new java.awt.Color(0,51,153));
                          setText (format.format(fecha));
                      }
                  });
