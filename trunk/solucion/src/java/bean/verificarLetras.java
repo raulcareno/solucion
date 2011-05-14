67f0862f1574604b769b7bfa0c9dedd576e3d6e1
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2011-05-14 04:32:44 +0000

    barras

---
+++ trunk/solucion/src/java/bean/verificarLetras.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import jcinform.persistencia.Letras;
+import jcinform.procesos.Administrador;
+
+/**
+ *
+ * @author Familia Jadan Cahueñ
+ */
+public class verificarLetras {
+
+    public void verificarLetras() {
+
+    }
+    public String verifica(String apellido,Administrador adm) {
+        List<Letras> letr = adm.query("Select o from Letras as o");
+        Letras letraEncontrada = null;
+        String primeraLetra ="";
+        try {
+                primeraLetra = apellido.replace(";","").replace(",", "").substring(0, 2).toUpperCase();
+        } catch (Exception e) {
+                primeraLetra = "XX";
+        }
+ 
+        Date fechaActual = new Date();
+        Date actual = (fechaActual);
+
+        if (primeraLetra.equals("CH")) {
+            letraEncontrada = fechas(letr, primeraLetra);
+            if (letraEncontrada != null) {
+                Date inicial = (letraEncontrada.getDesde());
+                Date finale = (letraEncontrada.getHasta());
+                System.out.println("" + letraEncontrada.getLetras());
+                if (actual.compareTo(finale) <= 0 && actual.compareTo(inicial) >= 0) {
+                    System.out.println("HABILITADO");
+                    return "OK..! DESDE: "+letraEncontrada.getDesde().toLocaleString()+" HASTA: "+letraEncontrada.getHasta().toLocaleString();
+                } else {
+                    System.out.println("DESHABILITADO");
+                    return "ERROR..! APELLIDOS CON [ "+letraEncontrada.getLetras().replace(";"," o ")+" ] ESTÁ(N) \n  DESDE: "+letraEncontrada.getDesde().toLocaleString()+" \n  HASTA: "+letraEncontrada.getHasta().toLocaleString();
+                }
+
+
+            } else {
+                return "ERROR...! \nLETRA INCIAL [ "+primeraLetra+" ] DE APELLIDO NO ENCONTRADA";
+            }
+        } else {
+            primeraLetra = apellido.substring(0, 1).toUpperCase();
+            letraEncontrada = fechas(letr, primeraLetra);
+            if (letraEncontrada != null) {
+                Date inicial = letraEncontrada.getDesde();
+                Date finale = letraEncontrada.getHasta();
+                if (actual.compareTo(finale) <= 0 && actual.compareTo(inicial) >= 0) {
+                    System.out.println("HABILITADO");
+                     return "OK..! DESDE: "+letraEncontrada.getDesde().toLocaleString()+" HASTA: "+letraEncontrada.getHasta().toLocaleString();
+                } else {
+                    System.out.println("DESHABILITADO");
+                    return "ERROR..! APELLIDOS CON [ "+letraEncontrada.getLetras().replace(";"," o ")+" ] ESTÁ(N) \n  DESDE: "+letraEncontrada.getDesde().toLocaleString()+" \n  HASTA: "+letraEncontrada.getHasta().toLocaleString();
+                }
+            } else {
+                return "ERROR...! \nLETRA INCIAL [ "+primeraLetra+" ] DE APELLIDO NO ENCONTRADA";
+            }
+
+
+        }
+    }
+
+    public Letras fechas(List<Letras> letr, String letraComprobar) {
+
+        for (Iterator<Letras> it = letr.iterator(); it.hasNext();) {
+            Letras letras = it.next();
+            if (letras.getLetras().contains(letraComprobar)) {
+                return letras;
+            }
+        }
+        return null;
+
+
+    }
+
+
+}
