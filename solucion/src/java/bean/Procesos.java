63b0eba3d0690a9133ada9e789a84c6df5560f6d
jcinform <jcinform@gmail.com>
2011-07-07 01:06:03 +0000

    29032011

--- solucion/src/java/bean/Procesos.java
+++ solucion/src/java/bean/Procesos.java
@@ -12,13 +12,13 @@ import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jcinform.persistencia.Cursos;
+import jcinform.persistencia.Global;
 import jcinform.persistencia.Matriculas;
 import jcinform.persistencia.Notanotas;
 import jcinform.persistencia.Notasrecord;
 import jcinform.persistencia.Periodo;
 import jcinform.procesos.Administrador;
 import org.zkoss.zhtml.Messagebox;
-import pruebas.NewMain;
 
 /**
  *
@@ -51,6 +51,7 @@ public class Procesos {
             List<Matriculas> matriculas = adm.query("Select o from Matriculas as o " +
                     "where o.curso.codigocur = '" + cursos.getCodigocur() + "' ");
             if (notas.size() > 0) {
+                if (matriculas.size() > 0) {
                 Notanotas no = notas.get(0);
                  System.out.println("CURSO: "+cursos.getDescripcion()+" "+ cursos.getEspecialidad());
                 for (Iterator<Matriculas> it = matriculas.iterator(); it.hasNext();) {
@@ -62,7 +63,7 @@ public class Procesos {
                             + "and cuantitativa = true  "
                             + "and materia != 0 "
                             + "and disciplina = false  ";
-                    System.out.println(que);
+                    //System.out.println(que);
                     List val = adm.queryNativo(que);
 
                     /*
@@ -114,6 +115,92 @@ public class Procesos {
                             nuevoR.setSexto(promedio);
                             adm.guardar(nuevoR);
                     }
+                }
+            }
+                System.out.println("------------------------------------------------");
+            } else {
+                try {
+                    Messagebox.show("Error no ha parametrizado el PF en la libreta");
+                    return;
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Procesos.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+    }
+     
+     public  void aprovechamiento2(Cursos cur, Global materia) {
+        Administrador adm = new Administrador();
+        List<Cursos> cursosList = new ArrayList<Cursos>();
+      Periodo periodo = cur.getPeriodo();
+        if (cur.getCodigocur().equals(-1)) {
+            cursosList = adm.query("Select o from Cursos as o where o.periodo.codigoper =  '"+periodo.getCodigoper()+"' ");
+        }else{
+          cur = (Cursos) adm.buscarClave(cur.getCodigocur(), Cursos.class);
+          cursosList.add(cur);
+        }
+
+        //Cursos cur = new Cursos(1);
+        List<Notanotas> notas = adm.query("Select o from Notanotas as o " +
+                " where o.sistema.periodo.codigoper = '" + periodo.getCodigoper() + "'  " +
+                "and o.sistema.promediofinal =  'PF'");
+
+        for (Iterator<Cursos> ist = cursosList.iterator(); ist.hasNext();) {
+            Cursos cursos = ist.next();
+
+            List<Matriculas> matriculas = adm.query("Select o from Matriculas as o " +
+                    "where o.curso.codigocur = '" + cursos.getCodigocur() + "' ");
+            if (notas.size() > 0) {
+                if (matriculas.size() > 0) {
+                Notanotas no = notas.get(0);
+                 System.out.println("CURSO: "+cursos.getDescripcion()+" "+ cursos.getEspecialidad()+" "+cursos.getParalelo().getDescripcion());
+                for (Iterator<Matriculas> it = matriculas.iterator(); it.hasNext();) {
+                    Matriculas matriculas1 = it.next();
+                    String que = "select " + no.getNota() + "  from notas " +
+                            "where matricula = '" + matriculas1.getCodigomat() + "' " +
+                            "and materia = '"+materia.getCodigo()+"' and disciplina = false ";
+                    //System.out.println(que);
+                    List val = adm.queryNativo(que);
+                      Double  promedio =  (Double) ((Vector)val.get(0)).get(0);
+                    if(promedio == null){
+                        promedio = 0.0;
+                    }
+                    List<Notasrecord> est = adm.query("Select o from Notasrecord as o " +
+                            "where o.estudiante.codigoest = '"+matriculas1.getEstudiante().getCodigoest()+"' ");
+                    if(est.size()>0){
+                        Notasrecord actualizaR = est.get(0);
+                        if(matriculas1.getCurso().getSecuencia().equals(1))
+                            actualizaR.setPrimero(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(2))
+                            actualizaR.setSegundo(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(3))
+                            actualizaR.setTercero(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(4))
+                            actualizaR.setCuarto(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(5))
+                            actualizaR.setQuinto(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(6))
+                            actualizaR.setSexto(promedio);
+                        adm.actualizar(actualizaR);
+                    }else{
+                        Notasrecord nuevoR = new Notasrecord();
+                        nuevoR.setEstudiante(matriculas1.getEstudiante());
+                            if(matriculas1.getCurso().getSecuencia().equals(1))
+                            nuevoR.setPrimero(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(2))
+                            nuevoR.setSegundo(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(3))
+                            nuevoR.setTercero(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(4))
+                            nuevoR.setCuarto(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(5))
+                            nuevoR.setQuinto(promedio);
+                        else if(matriculas1.getCurso().getSecuencia().equals(6))
+                            nuevoR.setSexto(promedio);
+                            adm.guardar(nuevoR);
+                    }
+                }
                 }
                 System.out.println("------------------------------------------------");
             } else {
