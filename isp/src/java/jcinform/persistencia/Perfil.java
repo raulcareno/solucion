e531ca7e7a472328f61ae82f77e7af777098cd04
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2011-07-29 04:34:36 +0000

    15072011

--- isp/src/java/jcinform/persistencia/Perfil.java
+++ isp/src/java/jcinform/persistencia/Perfil.java
@@ -101,7 +101,7 @@ public class Perfil implements Serializable {
 
     @Override
     public String toString() {
-        return "jcinform.persistencia.Perfil[codigo=" + codigo + "]";
+        return nombre;
     }
 
 }
