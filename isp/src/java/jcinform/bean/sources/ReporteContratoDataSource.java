d4dd4aaeb8426f3cc6fd65c694d8237201281f0d
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2011-08-03 21:47:53 +0000

    Cambio en contratos

---
+++ isp/src/java/jcinform/bean/sources/ReporteContratoDataSource.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jcinform.bean.sources;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import jcinform.conexion.Permisos;
+import jcinform.persistencia.Contratos;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+
+/**
+ *
+ * @author GEOVANNY
+ */
+public class ReporteContratoDataSource implements JRDataSource {
+
+    private Iterator itrAlumnos;
+    private Iterator itrNodos;
+    private Object valorAtual;
+    private boolean irParaProximoAlumno = true;
+
+    public ReporteContratoDataSource(List lista) {
+        super();
+        this.itrNodos = lista.iterator();
+    }
+
+    public boolean next() throws JRException {
+        itrAlumnos = itrNodos;
+        valorAtual = itrAlumnos.hasNext() ? itrAlumnos.next() : null;
+        irParaProximoAlumno = (valorAtual != null);
+        return irParaProximoAlumno;
+    }
+
+    public Object getFieldValue(JRField campo) throws JRException {
+        Object valor = null;
+
+        Contratos nodo = (Contratos) valorAtual;
+        String fieldName = campo.getName();
+        try {
+            if ("cliente".equals(fieldName)) {
+                valor = nodo.getClientes()+"";
+            } else if ("ruc".equals(fieldName)) {
+                valor = nodo.getClientes().getIdentificacion();
+            } else if ("direccion".equals(fieldName)) {
+                valor = nodo.getClientes().getDireccion();
+            } else if ("telefono".equals(fieldName)) {
+                valor = nodo.getClientes().getTelefono();
+            } else if ("contrato".equals(fieldName)) {
+                valor = nodo.getContrato();
+            } else if ("cedula".equals(fieldName)) {
+                valor = nodo.getClientes().getIdentificacion();
+            } else if ("usuario".equals(fieldName)) {
+                valor = nodo.getUsuario();
+            } else if ("clave".equals(fieldName)) {
+                valor = nodo.getClave();
+            } else if ("plan".equals(fieldName)) {
+                valor = nodo.getPlan()+"";
+            } else if ("sector".equals(fieldName)) {
+                valor = nodo.getRadios().getNodos().getSector().getNombre()+"";
+            } else if ("radio".equals(fieldName)) {
+                valor = nodo.getRadios().getNombre()+"";
+            } else if ("nodo".equals(fieldName)) {
+                valor = nodo.getRadios().getNodos().getNombre()+"";
+            } else if ("canton".equals(fieldName)) {
+                valor = nodo.getRadios().getNodos().getSector().getCanton().getNombre()+"";
+            } else if ("fecha".equals(fieldName)) {
+                valor = nodo.getFecha();
+            } else if ("fechainstalacion".equals(fieldName)) {
+                valor = nodo.getFechafinal();
+            }
+            
+        } catch (Exception e) {
+            System.out.println("en datasource Acta " + e);
+        }
+
+        return valor;
+    }
+    public int year = 0;
+    public int month = 0;
+    public int day = 0;
+
+    public void calcularEdad(Date nacimiento) {
+//Date yourDate = (Date) Calendar.getInstance().getTime();
+        java.sql.Timestamp birth = new java.sql.Timestamp(nacimiento.getTime());
+        Date d = new Date();
+
+
+        SimpleDateFormat sdfDia = new SimpleDateFormat("dd");
+        SimpleDateFormat sdfMes = new SimpleDateFormat("MM");
+        SimpleDateFormat sdfAño = new SimpleDateFormat("yyyy");
+
+        int a = Integer.parseInt(sdfAño.format(d)) - Integer.parseInt(sdfAño.format(birth));
+        int b = Integer.parseInt(sdfMes.format(d)) - Integer.parseInt(sdfMes.format(birth));
+        int c = Integer.parseInt(sdfDia.format(d)) - Integer.parseInt(sdfDia.format(birth));
+
+        if (b < 0) {
+            a = a - 1;
+            b = 12 + b;
+        }
+
+        if (c < 0) {
+            b = b - 1;
+            switch (Integer.parseInt(sdfMes.format(d))) {
+                case 2:
+                    int año = Integer.parseInt(sdfAño.format(d));
+                    if ((año % 4 == 0) && ((año % 100 != 0) || (año % 400 == 0))) {
+                        c = 29 + c;
+                    } else {
+                        c = 28 + c;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 10:
+                case 11:
+                    c = 30 + c;
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 12:
+                    c = 31 + c;
+                    break;
+            }
+        }
+
+
+        this.day = c;
+        this.month = (b < 0 ? 0 : b);
+        this.year = a;
+
+    }
+}
