626fc1ebc9b1fe815b829605eed772fef0b1392f
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-02-07 03:05:48 +0000

    Globales, parametros

---
+++ trunk/solucion/src/java/sources/ConteoMatriculas.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sources;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Familia Jadan Cahueñ
+ */
+public class ConteoMatriculas {
+    
+    public Integer conteo;
+    public String nombrecurso;
+    public String paralelo;
+    public String especialidad;
+    public String genero;
+    public Integer numero;
+     
+    
+    public ConteoMatriculas(){
+        
+         }
+
+    public Integer getConteo() {
+        return conteo;
+    }
+
+    public void setConteo(Integer conteo) {
+        this.conteo = conteo;
+    }
+
+  
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNombrecurso() {
+        return nombrecurso;
+    }
+
+    public void setNombrecurso(String nombrecurso) {
+        this.nombrecurso = nombrecurso;
+    }
+
+    public String getParalelo() {
+        return paralelo;
+    }
+
+    public void setParalelo(String paralelo) {
+        this.paralelo = paralelo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+  
+    
+}
