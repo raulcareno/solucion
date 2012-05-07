70e8fb480837a9bbc16a78256c65db3b42629e3e
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-05-07 22:04:07 +0000

    matriculascion por internet

--- isp/src/java/jcinform/bean/generarCM.java
+++ isp/src/java/jcinform/bean/generarCM.java
@@ -37,16 +37,16 @@ public class generarCM {
             Empleadossucursal sucursalEmp = (Empleadossucursal) ses.getAttribute("sector");
 
             byte[] data = null;
-            File outFile =  File.createTempFile("archivoBan", ".txt");
+            File outFile = File.createTempFile("archivoBan", ".txt");
             BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
             if (banco.getNombre().toLowerCase().contains("pichincha")) {
-              
+
                 for (Iterator it = datos.iterator(); it.hasNext();) {
                     Row object = (Row) it.next();
                     //Facturasenviadas fac = (Facturasenviadas) adm.buscarClave(new Integer(((Facturasenviadas)object.getValue()).getCodigo(), Facturasenviadas.class);
                     Facturasenviadas fac = (Facturasenviadas) object.getValue();
                     String factura = fac.getFactura().getNumero().replace("FAC", "-");
-                    factura = factura.substring(0,3)+"-"+factura.substring(3,6)+"-00"+factura.substring(7, factura.length());
+                    factura = factura.substring(0, 3) + "-" + factura.substring(3, 6) + "-00" + factura.substring(7, factura.length());
                     String valor = fac.getSaldo() + "";
                     String base = (fac.getSaldo().divide(new BigDecimal(1.12), 2, RoundingMode.HALF_UP)) + "";//BASE IVA TOCARÍA SACAR LA BASE IVA DE LO QUE SE ENVIARÍA
                     try {
@@ -66,15 +66,15 @@ public class generarCM {
                     } catch (Exception e) {
                     }
                     writer.write("CO" // cobro
-                            + "\t" + fac.getFactura().getContratos().getCodigo() // no de contrato
+                            + "\t" + fac.getFactura().getContratos().getContrato() // no de contrato
                             + "\tUSD" // moneda
                             + "\t" + valor // valor adeudado
-                            + "\tCTA\t" + (fac.getFactura().getContratos().getTipocuenta().equals("AHO")?"AHO":"CTE") // tipo de cuenta aho, cTE
+                            + "\tCTA\t" + (fac.getFactura().getContratos().getTipocuenta().equals("AHO") ? "AHO" : "CTE") // tipo de cuenta aho, cTE
                             + "\t" + fac.getFactura().getContratos().getNocuenta() // cuenta del banco
                             + "\tMENSUALIDAD " + mes(fac.getFecha().getMonth()) + "" + (fac.getFecha().getYear() + 1900) // descripcion nuestra
                             + "\t" + fac.getFactura().getContratos().getClientes().getTipoidentificacion() // tipo de documento
                             + "\t" + fac.getFactura().getContratos().getClientes().getIdentificacion().replace("-", "") // cedula
-                            + "\t" + ((fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres()).length() > 41 ? (fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres()).substring(0,41) :(fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres()))   // nombres cliente
+                            + "\t" + ((fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres()).length() > 41 ? (fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres()).substring(0, 41) : (fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres())) // nombres cliente
                             + "\t" + base //base del iva
                             + "\t"
                             + "\t"
@@ -88,25 +88,25 @@ public class generarCM {
                 }
 
             } else if (banco.getNombre().toLowerCase().contains("guayaquil")) {
-              
-               
+
+
                 for (Iterator it = datos.iterator(); it.hasNext();) {
                     Row object = (Row) it.next();
                     //Facturasenviadas fac = (Facturasenviadas) adm.buscarClave(new Integer(((Facturasenviadas)object.getValue()).getCodigo(), Facturasenviadas.class);
                     Facturasenviadas fac = (Facturasenviadas) object.getValue();
                     String valor = fac.getSaldo() + "";
                     String base = (fac.getSaldo().divide(new BigDecimal(1.12), 2, RoundingMode.HALF_UP)) + "";//BASE IVA TOCARÍA SACAR LA BASE IVA DE LO QUE SE ENVIARÍA
-                    String cuenta = fac.getFactura().getContratos().getNocuenta()  ;
+                    String cuenta = fac.getFactura().getContratos().getNocuenta();
                     try {
 
-                         
+
                         while (cuenta.length() < 10) {
                             cuenta = "0" + cuenta;
                         }
                     } catch (Exception e) {
                     }
 
-                            //NDEAAAAMMDDCCC_SE.TXT
+                    //NDEAAAAMMDDCCC_SE.TXT
                     try {
 
                         valor = valor.replace(",", "").replace(".", "");
@@ -123,17 +123,19 @@ public class generarCM {
                         }
                     } catch (Exception e) {
                     }
-                    writer.write(""+ fac.getFactura().getContratos().getTipocuenta().substring(0,1) //1 Tipo de cuenta	Alfanumérico	A/C	1	Tipo de cuenta del cliente.  A: Ahorros.  C: Corriente.
+                    writer.write("" + fac.getFactura().getContratos().getTipocuenta().substring(0, 1) //1 Tipo de cuenta	Alfanumérico	A/C	1	Tipo de cuenta del cliente.  A: Ahorros.  C: Corriente.
                             + "\t" + cuenta// Nro Cuenta Numérico 10	Número de cuenta del cliente a debitar
                             + "\t" + valor //3 Valor	Numérico 15 Valor a debitar.  13 enteros, 2 decimales.
                             + "\t" + "XX" //4 Constante	Alfanumérico	XX	2	Valor constante XX
-                            + "\t"+ "W"   //5	Tipo nota Alfanumérico	W	1	Tipo de transacción.  W: Débito en dólares.
-                            + "\t"+ "01"  //6	Agencia	Numérico 01/06	2 Tipo de agencia.  01: Matriz.  06: Sucursal. 01 si es por Banca Transaccional ( Internet)
-                            +"");
+                            + "\t" + "W" //5	Tipo nota Alfanumérico	W	1	Tipo de transacción.  W: Débito en dólares.
+                            + "\t" + "01" //6	Agencia	Numérico 01/06	2 Tipo de agencia.  01: Matriz.  06: Sucursal. 01 si es por Banca Transaccional ( Internet)
+                            + "");
                     writer.newLine(); // Esto es un salto de linea
                 }
 
             } else if (banco.getNombre().toLowerCase().contains("bolivariano")) {
+                int contador = 1;
+                String contadorString = "";
                 for (Iterator it = datos.iterator(); it.hasNext();) {
                     Row object = (Row) it.next();
                     //Facturasenviadas fac = (Facturasenviadas) adm.buscarClave(new Integer(((Facturasenviadas)object.getValue()).getCodigo(), Facturasenviadas.class);
@@ -143,7 +145,7 @@ public class generarCM {
                     try {
 
                         valor = valor.replace(",", "").replace(".", "");
-                        while (valor.length() < 11) {
+                        while (valor.length() < 15) {
                             valor = "0" + valor;
                         }
                     } catch (Exception e) {
@@ -155,26 +157,85 @@ public class generarCM {
                             base = "0" + base;
                         }
                     } catch (Exception e) {
+                    }//bolivariano
+                    try {
+                        contadorString = contador + "";
+                        while (contadorString.length() < 2) {
+                            contadorString = "0" + contador;
+                        }
+                    } catch (Exception e) {
                     }
-                    writer.write("CO" // cobro
-                            + "\t" + fac.getFactura().getContratos().getCodigo() // no de contrato
-                            + "\tUSD" // moneda
-                            + "\t" + valor // valor adeudado
-                            + "\tCTA\t" + fac.getFactura().getContratos().getTipocuenta() // tipo de cuenta aho, cor
-                            + "\t" + fac.getFactura().getContratos().getNocuenta() // cuenta del banco
-                            + "\tMENSUALIDAD " + mes(fac.getFecha().getMonth()) + "" + (fac.getFecha().getYear() + 1900) // descripcion nuestra
-                            + "\t" + fac.getFactura().getContratos().getClientes().getTipoidentificacion() // tipo de documento
-                            + "\t" + fac.getFactura().getContratos().getClientes().getIdentificacion().replace("-", "") // cedula
+                    //cedula
+                    String cedula = "" + fac.getFactura().getContratos().getClientes().getIdentificacion().replace("-", "");
+                    try {
+
+                        while (cedula.length() < 14) {
+                            cedula = "0" + cedula;
+                        }
+                    } catch (Exception e) {
+                    }
+                    //cuenta
+                    String nocuenta = "" + fac.getFactura().getContratos().getNocuenta();
+                    try {
+
+                        while (nocuenta.length() < 20) {
+                            nocuenta = "0" + nocuenta;
+                        }
+                    } catch (Exception e) {
+                    }//bolivariano
+
+                    String espacios35 = " ";
+                    try {
+
+                        while (espacios35.length() < 15) {
+                            espacios35 = " " + espacios35;
+                        }
+                    } catch (Exception e) {
+                    }//bolivariano
+                    String espacios50 = " ";
+                    try {
+
+                        while (espacios50.length() < 50) {
+                            espacios50 = " " + espacios50;
+                        }
+                    } catch (Exception e) {
+                    }//bolivariano
+                    String espacios31 = " ";
+                    try {
+
+                        while (espacios31.length() < 31) {
+                            espacios31 = " " + espacios31;
+                        }
+                    } catch (Exception e) {
+                    }//bolivariano
+
+                    writer.write("BZDET" // cobro
+                            + "\t" + contadorString //SECUENCIAL
+                            + "\t" + fac.getFactura().getContratos().getContrato() //NUMERO CONTRATO 
+                            + "\t" + fac.getFactura().getContratos().getClientes().getTipoidentificacion() // C, R, P
+                            + "\t" + cedula// cedula, ruc, pasaporte
                             + "\t" + fac.getFactura().getClientes().getApellidos() + " " + fac.getFactura().getClientes().getNombres() // nombres cliente
-                            + "\t" + base //base del iva
-                            + "\t"
-                            + "\t"
-                            + "\t"
-                            + "\t"
-                            + "\t"
-                            + "\t"
+                            + "\tCUE"// estatico
+                            + "\t001"// estatico
+                            + "\t34"// estatico
+                            + "\t" + (fac.getFactura().getContratos().getTipocuenta().equals("AHO") ? "04" : "03")
+                            + "\t" + nocuenta
+                            + "\t1"// estatico moneda 1
+                            + "\t" + valor // valor adeudado
+                            + "\tMENSUALIDAD " + mes(fac.getFecha().getMonth()) + "" + (fac.getFecha().getYear() + 1900) // descripcion nuestra
                             + "\t" + fac.getFactura().getNumero().replace("FAC", "")
-                            + "\t" + fac.getFactura().getAutorizacion());
+                            + "\t" + base //base del iva
+                            + "\t" + espacios35// en blanco
+                            + "\t" + "          "// en blanco 10 espacios
+                            + "\t" + espacios50
+                            + "\t" + espacios50
+                            + "\t" + "                    "// en blanco 20 espacios
+                            + "\tREC"
+                            + "\t" + espacios31
+                            + "\t" + banco.getEmpresa()
+                            + "\t" + valor
+                            + "\t001"); //tipo de comprobante 001 igual a factura
+
                     writer.newLine(); // Esto es un salto de linea
                 }
             }
