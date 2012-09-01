cc5a4f66a44e79c77cda4a7f0c27794d6d9b0cf1
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-09-01 12:26:09 +0000

    wed

--- usol/web/bases/locales.js
+++ usol/web/bases/locales.js
@@ -11,7 +11,7 @@ PrimeFaces.locales['es'] = {
     monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
     dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
     dayNamesShort: ['Dom','Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
-    dayNamesMin: ['D','L','M','X','J','V','S'],
+    dayNamesMin: ['D','L','M','M','J','V','S'],
     weekHeader: 'Semana',
     firstDay: 1,
     isRTL: false,
