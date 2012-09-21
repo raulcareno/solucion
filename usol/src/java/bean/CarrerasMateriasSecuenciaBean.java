6af980d0a9f2106bacee569696a5cc266b8bda11
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-09-21 01:59:05 +0000

    cst

--- usol/src/java/bean/CarrerasMateriasSecuenciaBean.java
+++ usol/src/java/bean/CarrerasMateriasSecuenciaBean.java
@@ -21,11 +21,12 @@ import jcinform.persistencia.CarrerasMaterias;
 import jcinform.persistencia.Ejes;
 import jcinform.persistencia.Materias;
 import jcinform.persistencia.Niveles;
+import jcinform.persistencia.SecuenciaDeMaterias;
 import jcinform.procesos.Administrador;
 import org.primefaces.event.DragDropEvent;
 
 import utilerias.Permisos;
- 
+
 /**
  *
  * @author Geovanny
@@ -46,14 +47,9 @@ public class CarrerasMateriasSecuenciaBean {
     protected Materias materiasSeleccionada;
     public String textoBuscar;
     Permisos permisos;
-Auditar  aud = new Auditar();
+    Auditar aud = new Auditar();
+
     public CarrerasMateriasSecuenciaBean() {
-        //super();
-//        FacesContext context = FacesContext.getCurrentInstance();
-//        String s = context.getExternalContext().getRequestParameterMap().get("skp");
-//        if (s != null) {
-//            System.out.println(s);
-//        }
         if (adm == null) {
             adm = new Administrador();
         }
@@ -70,56 +66,99 @@ Auditar  aud = new Auditar();
 //                Logger.getLogger(CarrerasMateriasBean.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-      
+llenarArreglo();
         //selectedCarrerasMaterias = new CarrerasMaterias();
 
     }
-    public void onCarrerasInvDrop(CarrerasMaterias player) {  
-        
-//        anadidas.remove(player);
 
-     
+    public String guardar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+//            if (!permisos.verificarPermisoReporte("Facultad", "agregar_facultad", "agregar", true, "PARAMETROS")) {
+//                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));                return null;
+//            }
+        try {
+             //borro primero las anteriores
+                adm.ejecutaSql("Delete from SecuenciaDeMaterias where idCarrerasMaterias.idCarreras.idCarreras = '"+carreraSeleccionada.getIdCarreras()+"' ");
+                for (int i = 0; i < 30; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        CarrerasMaterias carreraMateria = anadidasArray[i][j];
+                        SecuenciaDeMaterias sec = new SecuenciaDeMaterias(adm.getNuevaClave("SecuenciaDeMaterias", "idSecuenciaDeMaterias"));
+                        sec.setIdCarrerasMaterias(carreraMateria); 
+                        sec.setFila(i);
+                        sec.setOrden(j);
+                        
+                        if(carreraMateria.getIdCarreras().getIdCarreras()!=null){
+                            adm.guardar(sec);
+                        } 
+                        
+                    }
+                }
+                aud.auditar(adm, this.getClass().getSimpleName().replace("Bean", ""), "guardar", "", carreraSeleccionada.getNombre() + "");
+                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage("Guardado...!"));
+             
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
+        }
+
+
+        return null;
+    }
+
+    /**
+     * ELIMINO EL ELEMENTO SELECCIONADO EVENTO SE REALIZADA EN EL DRAG DROP
+     * AÑADO
+     *
+     * @param player
+     */
+    public void onCarrerasInvDrop(CarrerasMaterias player) {
+
+
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j < 12; j++) {
                 CarrerasMaterias carA = anadidasArray[i][j];
-                if(player.equals(carA)){
-                  CarrerasMaterias car = new CarrerasMaterias();
-                        Materias mat = new Materias();
-                        mat.setNombre("");
-                        car.setIdMaterias(mat);
-                        car.setIdNiveles(new Niveles());
-                        car.setIdCarreras(new Carreras());   
-                        anadidasArray[i][j] = car;
-                        listaMaterias.add(player); 
+                if (player.equals(carA)) {
+                    CarrerasMaterias car = new CarrerasMaterias();
+                    Materias mat = new Materias();
+                    mat.setNombre("");
+                    car.setIdMaterias(mat);
+                    car.setIdNiveles(new Niveles());
+                    car.setIdCarreras(new Carreras());
+                    anadidasArray[i][j] = car;
+                    listaMaterias.add(player);
                 }
-                
+
             }
-            
+
         }
-        
-    
-        
-        
+
+
+
+
     }
-   public void onCarrerasDrop(DragDropEvent event) {  
-        CarrerasMaterias player = (CarrerasMaterias) event.getData();  
-        anadidas.add(player); 
+
+    /**
+     * AÑADO A LA MALLA LAS MATERIAS
+     *
+     * @param event
+     */
+    public void onCarrerasDrop(DragDropEvent event) {
+        CarrerasMaterias player = (CarrerasMaterias) event.getData();
+        //anadidas.add(player); 
         listaMaterias.remove(player);
-         String filaColumna = event.getDropId();
-         Integer fila = new Integer(filaColumna.substring(filaColumna.indexOf("f")+1, filaColumna.indexOf("c")));
-         Integer columna = new Integer(filaColumna.substring(filaColumna.indexOf("c")+1, filaColumna.length()));
-        anadidasArray[fila][columna] =  player;
-        //System.out.println("ID EN LA CUAL SE HA HAÑADIDO"+event.getDropId());
-        //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(player.getIdCarreras().getNombre() + " added", "Position:" + event.getDropId()));  
-    }   
+        String filaColumna = event.getDropId();
+        Integer fila = new Integer(filaColumna.substring(filaColumna.indexOf("f") + 1, filaColumna.indexOf("c")));
+        Integer columna = new Integer(filaColumna.substring(filaColumna.indexOf("c") + 1, filaColumna.length()));
+        anadidasArray[fila][columna] = player;
+    }
     /**
      * busca según criterio textoBuscar
      */
     List<CarrerasMaterias> listaMaterias = new ArrayList<CarrerasMaterias>();
-    List<CarrerasMaterias> anadidas  = new ArrayList<CarrerasMaterias>();
+    List<CarrerasMaterias> anadidas = new ArrayList<CarrerasMaterias>();
     //List<CarrerasMaterias> anadidas2[2][2]  = new ArrayList<>();
-   CarrerasMaterias anadidasArray[][] = new CarrerasMaterias[30][12];
-    public void llenarArreglo(){
+    CarrerasMaterias anadidasArray[][] = new CarrerasMaterias[30][12];
+
+    public void llenarArreglo() {
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j < 12; j++) {
                 CarrerasMaterias car = new CarrerasMaterias();
@@ -130,16 +169,64 @@ Auditar  aud = new Auditar();
                 car.setIdCarreras(new Carreras());
                 anadidasArray[i][j] = car;
             }
-            
+
         }
-        
+
     }
+
     public void buscarMateriasdeCarrera() {
         try {
-        
-              listaMaterias = adm.query("Select o from CarrerasMaterias as o "
-                        + " where o.idCarreras.idCarreras = '" + carreraSeleccionada.getIdCarreras() + "' ");
-          llenarArreglo();
+            llenarArreglo();
+            listaMaterias = adm.query("Select o from CarrerasMaterias as o "
+                    + " where o.idCarreras.idCarreras = '" + carreraSeleccionada.getIdCarreras() + "'  "
+                    + "and o.idCarrerasMaterias NOT IN (Select c.idCarrerasMaterias.idCarrerasMaterias "
+                    + "from SecuenciaDeMaterias as c "
+                    + " WHERE c.idCarrerasMaterias.idCarreras.idCarreras = '" + carreraSeleccionada.getIdCarreras() + "' ) "
+                    + " order by o.idNiveles.secuencia ");
+            List<SecuenciaDeMaterias> materiasSecuenciales = adm.query("Select o from SecuenciaDeMaterias as o "
+                    + "where o.idCarrerasMaterias.idCarreras.idCarreras = '" + carreraSeleccionada.getIdCarreras() + "' "
+                    + "order by o.fila, o.orden ");
+            if (materiasSecuenciales.size() > 0) {
+                for (Iterator<SecuenciaDeMaterias> it = materiasSecuenciales.iterator(); it.hasNext();) {
+                    SecuenciaDeMaterias cM = it.next();
+                    anadidasArray[cM.getFila()][cM.getOrden()] = cM.getIdCarrerasMaterias();
+                }
+                //LLENO LOS VACIOS
+                 for (int i = 0; i < 30; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        try {
+                            CarrerasMaterias tmp = anadidasArray[i][j];    
+                            if(tmp.getIdCarrerasMaterias().equals(null)){
+                                CarrerasMaterias car = new CarrerasMaterias();
+                                Materias mat = new Materias();
+                                mat.setNombre("");
+                                car.setIdMaterias(mat);
+                                car.setIdNiveles(new Niveles());
+                                car.setIdCarreras(new Carreras());
+                                anadidasArray[i][j] = car;
+                            }
+                                
+                            
+                        } catch (Exception e) {
+                            CarrerasMaterias car = new CarrerasMaterias();
+                            Materias mat = new Materias();
+                            mat.setNombre("");
+                            car.setIdMaterias(mat);
+                            car.setIdNiveles(new Niveles());
+                            car.setIdCarreras(new Carreras());
+                            anadidasArray[i][j] = car;
+                        }
+                        
+                         
+                            
+                         
+                    }
+
+                }
+                
+            } else {
+                llenarArreglo();
+            }
 
         } catch (Exception e) {
             java.util.logging.Logger.getLogger(CarrerasMateriasBean.class.getName()).log(Level.SEVERE, null, e);
@@ -150,17 +237,17 @@ Auditar  aud = new Auditar();
         try {
             List<Carreras> divisionPoliticas = new ArrayList<Carreras>();
             List<SelectItem> items = new ArrayList<SelectItem>();
-                    divisionPoliticas = adm.query("Select o from Carreras as o order by o.nombre ");
-                    if (divisionPoliticas.size() > 0) {
-                        Carreras objSel = new Carreras(0);
-                        items.add(new SelectItem(objSel, "Seleccione..."));
-                        for (Carreras obj : divisionPoliticas) {
-                            items.add(new SelectItem(obj, obj.getNombre()));
-                        }
-                    } else {
-                        Carreras obj = new Carreras(0);
-                        items.add(new SelectItem(obj, "NO EXISTEN CARRERAS"));
-                    }
+            divisionPoliticas = adm.query("Select o from Carreras as o order by o.nombre ");
+            if (divisionPoliticas.size() > 0) {
+                Carreras objSel = new Carreras(0);
+                items.add(new SelectItem(objSel, "Seleccione..."));
+                for (Carreras obj : divisionPoliticas) {
+                    items.add(new SelectItem(obj, obj.getNombre()));
+                }
+            } else {
+                Carreras obj = new Carreras(0);
+                items.add(new SelectItem(obj, "NO EXISTEN CARRERAS"));
+            }
             return items;
         } catch (Exception e) {
             java.util.logging.Logger.getLogger(CarrerasMateriasBean.class.getName()).log(Level.SEVERE, null, e);
@@ -168,7 +255,6 @@ Auditar  aud = new Auditar();
         }
         return null;
     }
-      
 
     /**
      * propiedades
@@ -176,7 +262,7 @@ Auditar  aud = new Auditar();
      * @return
      */
     public List<CarrerasMaterias> getModel() {
-       
+
         return model;
     }
 
@@ -280,7 +366,4 @@ Auditar  aud = new Auditar();
     public void setAnadidasArray(CarrerasMaterias[][] anadidasArray) {
         this.anadidasArray = anadidasArray;
     }
-    
-    
-    
 }
