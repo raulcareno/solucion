f4062ee511262841d2650b1f0f004a001fc32d30
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-10-15 04:20:59 +0000

    cst

--- usol/src/java/reportes/bean/MatriculasDataModel.java
+++ usol/src/java/reportes/bean/MatriculasDataModel.java
@@ -8,12 +8,15 @@ package reportes.bean;
  *
  * @author inform
  */
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
-import javax.faces.model.ListDataModel;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModelListener;
 import jcinform.persistencia.Matriculas;
-import org.primefaces.model.SelectableDataModel;
-
-public class MatriculasDataModel extends ListDataModel<Matriculas> implements SelectableDataModel<Matriculas> {
+import org.primefaces.component.datatable.DataTable;
+ 
+public class MatriculasDataModel extends PrimeDataModel<Matriculas> {
 
     public MatriculasDataModel() {
     }
@@ -31,8 +34,40 @@ public class MatriculasDataModel extends ListDataModel<Matriculas> implements Se
         return null;
     }
 
+    
+ 
     @Override
     public Object getRowKey(Matriculas car) {
         return car.getIdMatriculas()+"";
     }
+    
+      public boolean isValueBlank(String value) {
+                if(value == null)
+                        return true;
+
+                return value.trim().equals("");
+        }
+
+       void decodeMultipleSelection(DataTable table, String selection) {
+                Class<?> clazz = table.getValueExpression("selection").getType(FacesContext.getCurrentInstance().getELContext());
+
+                if(isValueBlank(selection)) {
+                        Object data = Array.newInstance(clazz.getComponentType(), 0);
+                        table.setSelection(data);   
+                }
+        else {
+            String[] rowKeys = selection.split(",");
+            List selectionList = new ArrayList();
+            
+            for(int i = 0; i < rowKeys.length; i++) {
+                Object rowData = table.getRowData(rowKeys[i]);
+                
+                if(rowData != null)
+                    selectionList.add(rowData);
+            }
+
+            Object selectinArray = Array.newInstance(clazz.getComponentType(), selectionList.size());
+            table.setSelection(selectionList.toArray((Object[])selectinArray));
+                }
+        }
 }
--- usol/src/java/reportes/bean/ReportesBean.java
+++ usol/src/java/reportes/bean/ReportesBean.java
@@ -6,9 +6,12 @@ package reportes.bean;
 
 import bean.*;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -23,6 +26,8 @@ import jcinform.persistencia.Matriculas;
 import jcinform.persistencia.Pais;
 import jcinform.persistencia.Provincia;
 import jcinform.procesos.Administrador;
+import reportes.ExportarReportes;
+import reportes.FichaMatricula;
 
 import utilerias.Permisos;
 
@@ -32,17 +37,15 @@ import utilerias.Permisos;
  */
 @ManagedBean
 @ViewScoped
-public class ReportesBean {
+public class ReportesBean implements Serializable {
 
     Administrador adm;
     Permisos permisos;
     Auditar aud = new Auditar();
- private Matriculas selectedMatricula;  
-  
-    private Matriculas[] selectedMatriculas;  
-  
-    private MatriculasDataModel mediumMatriculasModel;  
-  
+    private Matriculas selectedMatricula;
+    private Matriculas[] selectedMatriculas;
+    private List<Matriculas> mediumMatriculasModel;
+
     public ReportesBean() {
         //super();
         FacesContext context = FacesContext.getCurrentInstance();
@@ -63,6 +66,7 @@ public class ReportesBean {
 //                Logger.getLogger(InstitucionBean.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        //selectedMatricula = new Matriculas();
         //selectedInstitucion = new Institucion();
 
     }
@@ -80,52 +84,53 @@ public class ReportesBean {
         }
         return null;
     }
-    public void buscarMatriculas(){
-        
-        try {
-            List<Matriculas> matriculasEncontradas = new ArrayList<Matriculas>();
-            
+    public Boolean todos;
 
-//                if (!object.getIdCanton().equals("")) {
-                try {
-                    matriculasEncontradas = adm.query("Select o from Matriculas as o "
-                            + "where o.idCarreras.idCarreras = '" + carreraSeleccionado.getIdCarreras() + "' order by o.idEstudiantes.apellidoPaterno ");
-                    if(matriculasEncontradas.size()>0){
-//                        Matriculas mat = new Matriculas(-1);
-//                        mat.setIdEstudiantes(new Estudiantes());
-//                        mat.getIdEstudiantes().setApellidoPaterno("[ T O D O S ]");
-//                        mat.getIdEstudiantes().setApellidoMaterno(" ");
-//                        mat.getIdEstudiantes().setNombre(" ");
-//                        matriculasEncontradas.add(mat);
-                    }
-                    mediumMatriculasModel = new MatriculasDataModel(matriculasEncontradas);    
-                } catch (Exception e) {
-                    System.out.println("NO SE HAN ENCONTRADO MATRICULADOS");
+    public void seleccionarTodos() {
+        for (Iterator<Matriculas> it = mediumMatriculasModel.iterator(); it.hasNext();) {
+            Matriculas matriculas = it.next();
+            matriculas.setConfirmada(todos);
+        }
+    }
+
+    public void buscarMatriculas() {
+
+        try {
+            try {
+                mediumMatriculasModel = adm.query("Select o from Matriculas as o "
+                        + "where o.idCarreras.idCarreras = '" + carreraSeleccionado.getIdCarreras() + "' order by o.idEstudiantes.apellidoPaterno ");
+                for (Iterator<Matriculas> it = mediumMatriculasModel.iterator(); it.hasNext();) {
+                    Matriculas matriculas = it.next();
+                    matriculas.setConfirmada(false);
                 }
+            } catch (Exception e) {
+                System.out.println("NO SE HAN ENCONTRADO MATRICULADOS");
+            }
         } catch (Exception e) {
             java.util.logging.Logger.getLogger(CantonBean.class.getName()).log(Level.SEVERE, null, e);
         }
-    
-        
+
+
     }
+
     public List<SelectItem> getSelectedItemCarreras() {
         try {
             List<Carreras> carrerasListado = new ArrayList<Carreras>();
             List<SelectItem> items = new ArrayList<SelectItem>();
-             
-                carrerasListado = adm.query("Select o from Carreras as o "
-                        + " order by o.nombre ");
-                if (carrerasListado.size() > 0) {
-                    Carreras objSel = new Carreras(0);
-                    items.add(new SelectItem(objSel, "Seleccione..."));
-                    for (Carreras obj : carrerasListado) {
-                        items.add(new SelectItem(obj, obj.getNombre()));
-                    }
-                } else {
-                    Carreras obj = new Carreras(0);
-                    items.add(new SelectItem(obj, "NO EXISTEN CARRERAS"));
+
+            carrerasListado = adm.query("Select o from Carreras as o "
+                    + " order by o.nombre ");
+            if (carrerasListado.size() > 0) {
+                Carreras objSel = new Carreras(0);
+                items.add(new SelectItem(objSel, "Seleccione..."));
+                for (Carreras obj : carrerasListado) {
+                    items.add(new SelectItem(obj, obj.getNombre()));
                 }
-             
+            } else {
+                Carreras obj = new Carreras(0);
+                items.add(new SelectItem(obj, "NO EXISTEN CARRERAS"));
+            }
+
             return items;
         } catch (Exception e) {
             java.util.logging.Logger.getLogger(MatriculasBean.class.getName()).log(Level.SEVERE, null, e);
@@ -133,7 +138,50 @@ public class ReportesBean {
         }
         return null;
     }
-        protected String tipoReporte;
+
+    public String verReporte(String tipo) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            List<Estudiantes> estu = new ArrayList<Estudiantes>();
+            Map map = new HashMap();
+            if (tipoReporte.equals("")) {
+                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Seleccione un Reporte...!", "Seleccione un Reporte...!"));
+                return null;
+            } else if (tipoReporte.equals("FM")) { //Ficha de Matricula" itemValue="FM" />
+                if (mediumMatriculasModel.size()  <= 0) {
+                    FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Seleccione un Estudiante...!", "Seleccione un Estudiante...!"));
+                    return null;
+                }
+                for (int i = 0; i < mediumMatriculasModel.size() ; i++) {
+                    if(mediumMatriculasModel.get(i).getConfirmada())
+                        estu.add(mediumMatriculasModel.get(i).getIdEstudiantes());
+                }
+                map.put("tituloReporte", "FICHA DE MATRICULA");
+                map.put("titulo1", "USUARIO");
+                map.put("titulo2", "# DE INFORMES");
+            }
+
+
+
+
+            ExportarReportes ex = new ExportarReportes(estu, "fichaMatricula", map);
+            if (tipo.equals("PDF")) {
+                ex.PDF();
+            } else if (tipo.equals("DOCX")) {
+                ex.DOCX();
+            } else if (tipo.equals("XLS")) {
+                ex.XLSX();
+            } else if (tipo.equals("PRINT")) {
+                ex.PRINT();
+            }
+        } catch (Exception ex1) {
+            FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, ex1.getMessage(), ex1.getMessage()));
+            java.util.logging.Logger.getLogger(FichaMatricula.class.getName()).log(Level.SEVERE, null, ex1);
+        }
+        return null;
+
+    }
+    protected String tipoReporte;
 
     /**
      * Get the value of tipoReporte
@@ -169,12 +217,11 @@ public class ReportesBean {
         this.selectedMatriculas = selectedMatriculas;
     }
 
-    public MatriculasDataModel getMediumMatriculasModel() {
-     
+    public List<Matriculas> getMediumMatriculasModel() {
         return mediumMatriculasModel;
     }
 
-    public void setMediumMatriculasModel(MatriculasDataModel mediumMatriculasModel) {
+    public void setMediumMatriculasModel(List<Matriculas> mediumMatriculasModel) {
         this.mediumMatriculasModel = mediumMatriculasModel;
     }
     private Carreras carreraSeleccionado;
@@ -197,4 +244,11 @@ public class ReportesBean {
         this.carreraSeleccionado = carreraSeleccionado;
     }
 
+    public Boolean getTodos() {
+        return todos;
+    }
+
+    public void setTodos(Boolean todos) {
+        this.todos = todos;
+    }
 }
