896e1ec5a27ca1e4d9acb96edcbef30d2fd04d42
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2012-11-18 05:17:47 +0000

    cst

--- usol/src/java/bean/NotasBean.java
+++ usol/src/java/bean/NotasBean.java
@@ -5,9 +5,13 @@
 package bean;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 import java.util.logging.Level;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -21,7 +25,11 @@ import jcinform.persistencia.Horarios;
 import jcinform.persistencia.Materias;
 import jcinform.persistencia.Niveles;
 import jcinform.persistencia.Notas;
+import jcinform.persistencia.Periodos;
+import jcinform.persistencia.SistemaNotas;
 import jcinform.procesos.Administrador;
+import org.joda.time.DateMidnight;
+import utilerias.NotasIngresar;
 
 import utilerias.Permisos;
 import utilerias.secuencial;
@@ -41,34 +49,30 @@ public class NotasBean {
     Administrador adm;
     protected List<Notas> model;
     public String textoBuscar;
+    protected Materias materiasSeleccionada;
     Permisos permisos;
     Auditar aud = new Auditar();
+    Periodos per = null;
 
     public NotasBean() {
         //super();
         FacesContext context = FacesContext.getCurrentInstance();
-//        String s = context.getExternalContext().getRequestParameterMap().get("skp");
-//        if (s != null) {
-//            System.out.println(s);
-//        }
+        per = (Periodos) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("periodo");
         if (adm == null) {
             adm = new Administrador();
         }
+        cabeceras = adm.query("Select o from SistemaNotas as o where o.idPeriodos.idPeriodos = '" + per.getIdPeriodos() + "' ");
         if (permisos == null) {
             permisos = new Permisos();
         }
-        if (!permisos.verificarPermisoReporte("Notas", "ingresar_pais", "ingresar", true, "PARAMETROS")) {
+        if (!permisos.verificarPermisoReporte("Notas", "ingresar_notas", "ingresar", true, "PARAMETROS")) {
             try {
                 FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "No tiene permisos para ingresar"));
                 FacesContext.getCurrentInstance().getExternalContext().redirect("noPuedeIngresar.jspx");
-            } //selectedNotas = new Notas();
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 java.util.logging.Logger.getLogger(NotasBean.class.getName()).log(Level.SEVERE, null, ex);
-//                Logger.getLogger(NotasBean.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        //selectedNotas = new Notas();
-
     }
 
     public String editarAction(Notas obj) {
@@ -82,6 +86,119 @@ public class NotasBean {
     protected void inicializar() {
         object = new Notas("0");
         cargarDataModel();
+
+    }
+    List listaNotas = new ArrayList();
+    List cabeceras = new ArrayList();
+
+    public String buscarNotas() {
+        boolean interno = true;
+        listaNotas = new ArrayList();
+        List<SistemaNotas> sistemas = adm.query("Select o from SistemaNotas as o "
+                + "where o.idPeriodos.idPeriodos = '" + per.getIdPeriodos() + "' "
+                + "order by o.idSistemaNotas ");
+//        for (Iterator<SistemaNotas> its = sistemas0.iterator(); its.hasNext();) {
+//            SistemaNotas acaNotanotas = its.next();
+//            if (!acaNotanotas.getFormula().trim().equals("")) {
+//                if (verificar(acaNotanotas.getFormula().trim(), false) == false) {
+//                    mensajes = "NO SE HA PROCEDIDO A CARGAR LAS NOTAS, LAS FORMULAS INGRESADAS EN APROVECHAMIENTO ESTÀN INCORRECTAS ...!";
+//                    return;
+//                }
+//            }
+//        }
+
+        String query = "";
+        for (SistemaNotas notass : sistemas) {
+            query += notass.getNota() + ",";
+        }
+        query = query.substring(0, query.length() - 1).replace("'", "").replace("(", "").replace(")", "");
+        int tamanio = sistemas.size();
+        String q = "SELECT matricula.estado_mat, matricula.id_matriculas, "
+                + "CONCAT(estudiantes.apellido_paterno,' ',estudiantes.apellido_materno,' ',estudiantes.nombre),  " + query.toUpperCase() + "   "
+                + " FROM Matriculas matricula  "
+                + "LEFT JOIN  Estudiantes estudiantes ON matricula.id_estudiantes = estudiantes.id_estudiantes  "
+                + "LEFT JOIN Materias_matricula mm ON matricula.id_matriculas = mm.id_matriculas AND mm.id_materias = '" + materiasSeleccionada.getIdMaterias() + "' "
+                + "LEFT JOIN Notas notas ON mm.id_materias_matricula  = notas.id_materias_matricula   "
+                + "   WHERE  matricula.estado_mat IN ('M','P','R') "
+                + "    ORDER BY estudiantes.apellido_paterno, estudiantes.apellido_materno, estudiantes.nombre";
+//        System.out.println("" + q);
+        List nativo = adm.queryNativo(q);
+        Date fechaActual = new Date();
+        DateMidnight actual = new DateMidnight(fechaActual);
+        for (Iterator itna = nativo.iterator(); itna.hasNext();) {
+            Object[] vec = (Object[]) itna.next();
+            int a = 0;
+            int x = 0;
+            ArrayList notaAnadir = new ArrayList();
+            Boolean estadoNota = false;
+            for (a = 0; a < vec.length; a++) {
+                SistemaNotas tnota = sistemas.get(x);
+                NotasIngresar n = new NotasIngresar();
+                if (a == 2) {
+                    String object = (String) vec[a];
+                    n.setNombre(object);
+                    n.setTexto(true);
+                    n.setNota(null);
+                    if (estadoNota) {
+                        n.setColorEstado("red");
+                    }
+                } else if (a == 1) {
+                    Integer object = (Integer) vec[a];
+                    n.setNombre(object + "");
+                    n.setTexto(true);
+                    n.setNota(null);
+                } else if (a == 0) {
+                    String object = (String) vec[a];
+
+                    n.setTexto(true);
+                    n.setNota(null);
+                    if (object.equals("M") || object.equals("P")) {
+                        n.setEstado(false);//DESHABILITO LA NOTA
+                    } else {
+                        estadoNota = true;
+                        n.setEstado(true);//DESHABILITO LA NOTA
+                        n.setNombre("");
+                    }
+                } else {
+
+                    DateMidnight inicial = new DateMidnight(tnota.getFechai());
+                    DateMidnight finale = new DateMidnight(tnota.getFechaf());
+                    if (interno) {
+                        inicial = new DateMidnight(tnota.getFechasi());
+                        finale = new DateMidnight(tnota.getFechasf());
+                    }
+
+                    if (actual.compareTo(finale) <= 0 && actual.compareTo(inicial) >= 0) {
+                        n.setEstado(false);//habilito la notaIngresada
+                        n.setColor("white");
+                    } else {
+                        n.setEstado(true);// deshabilita la notaIngresada
+                        n.setColor("#E9E7E2");
+                    }
+                    if (estadoNota) {
+                        n.setEstado(estadoNota);
+                        n.setColor("#E9E7E2");
+
+                    }
+
+                    Double object = (Double) vec[a];
+                    if (object == null) {
+                        object = 0.0;
+                    }
+                    n.setDesde(0.0);
+                    n.setHasta(10d);
+                    n.setNota(redondear(object, 2));
+                    n.setNombre("");
+                    n.setTexto(false);
+                    x++;
+                }
+                if(a>0)
+                notaAnadir.add(n);
+            }
+            listaNotas.add(notaAnadir);
+        }
+
+        return null;
     }
 
     /**
@@ -94,7 +211,7 @@ public class NotasBean {
 //            return null;
 //        }
 //        if (object.getIdNotas() == 0) {
-        if (!permisos.verificarPermisoReporte("Notas", "agregar_pais", "agregar", true, "PARAMETROS")) {
+        if (!permisos.verificarPermisoReporte("Notas", "agregar_notas", "agregar", true, "PARAMETROS")) {
             FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));
             return null;
         }
@@ -109,7 +226,7 @@ public class NotasBean {
             FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
         }
 //        } else {
-//            if (!permisos.verificarPermisoReporte("Notas", "actualizar_pais", "agregar", true, "PARAMETROS")) {
+//            if (!permisos.verificarPermisoReporte("Notas", "actualizar_notas", "agregar", true, "PARAMETROS")) {
 //                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));                return null;
 //            }
 //            try {
@@ -134,7 +251,7 @@ public class NotasBean {
     public String eliminar(Notas obj) {
         FacesContext context = FacesContext.getCurrentInstance();
         try {
-            if (!permisos.verificarPermisoReporte("Notas", "eliminar_pais", "eliminar", true, "PARAMETROS")) {
+            if (!permisos.verificarPermisoReporte("Notas", "eliminar_notas", "eliminar", true, "PARAMETROS")) {
                 FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));
                 return null;
             }
@@ -169,8 +286,37 @@ public class NotasBean {
 //        }
         return null;
     }
-    
-     public List<SelectItem> getSelectedItemNiveles() {
+
+    public List<SelectItem> getSelectedItemMaterias() {
+        try {
+            Periodos per = (Periodos) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("periodo");
+            List<Materias> divisionPoliticas = new ArrayList<Materias>();
+            List<SelectItem> items = new ArrayList<SelectItem>();
+
+
+            divisionPoliticas = adm.query("Select distinct o.idMaterias from Horarios as o "
+                    + " where o.idPeriodos.idPeriodos = '" + per.getIdPeriodos() + "' "
+                    + " order by o.idMaterias.nombre ");
+            if (divisionPoliticas.size() > 0) {
+                Materias objSel = new Materias(0);
+                items.add(new SelectItem(objSel, "Seleccione..."));
+                for (Materias obj : divisionPoliticas) {
+                    items.add(new SelectItem(obj, obj.getNombre()));
+                }
+            } else {
+                Materias obj = new Materias(0);
+                items.add(new SelectItem(obj, "NO EXISTEN NIVELES"));
+            }
+
+
+            return items;
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(Horarios.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+
+    public List<SelectItem> getSelectedItemNiveles() {
         try {
             List<Niveles> divisionPoliticas = new ArrayList<Niveles>();
             List<SelectItem> items = new ArrayList<SelectItem>();
@@ -221,32 +367,7 @@ public class NotasBean {
         }
         return null;
     }
-     public List<SelectItem> getSelectedItemMaterias() {
-        try {
-            List<Materias> divisionPoliticas = new ArrayList<Materias>();
-            List<SelectItem> items = new ArrayList<SelectItem>();
 
-
-            divisionPoliticas = adm.query("Select o from Materias as o order by o.nombre ");
-            if (divisionPoliticas.size() > 0) {
-                Materias objSel = new Materias(0);
-                items.add(new SelectItem(objSel, "Seleccione..."));
-                for (Materias obj : divisionPoliticas) {
-                    items.add(new SelectItem(obj, obj.getNombre()));
-                }
-            } else {
-                Materias obj = new Materias(0);
-                items.add(new SelectItem(obj, "NO EXISTEN NIVELES"));
-            }
-
-
-            return items;
-        } catch (Exception e) {
-            java.util.logging.Logger.getLogger(Horarios.class.getName()).log(Level.SEVERE, null, e);
-        }
-        return null;
-    }
-    
     public List<SelectItem> getSelectedItemCarreras() {
         try {
             List<Carreras> divisionPoliticas = new ArrayList<Carreras>();
@@ -269,7 +390,6 @@ public class NotasBean {
         }
         return null;
     }
-
 
     /**
      * llena el listado con datos
@@ -301,6 +421,16 @@ public class NotasBean {
         } catch (Exception e) {
             java.util.logging.Logger.getLogger(NotasBean.class.getName()).log(Level.SEVERE, null, e);
             System.out.println("" + e);
+        }
+    }
+
+    public Double redondear(Double numero, int decimales) {
+        try {
+            BigDecimal d = new BigDecimal(numero);
+            d = d.setScale(decimales, RoundingMode.HALF_UP);
+            return d.doubleValue();
+        } catch (Exception e) {
+            return 0.0;
         }
     }
 
@@ -359,5 +489,29 @@ public class NotasBean {
 
     public void setObject(Notas object) {
         this.object = object;
+    }
+
+    public List getListaNotas() {
+        return listaNotas;
+    }
+
+    public void setListaNotas(List listaNotas) {
+        this.listaNotas = listaNotas;
+    }
+
+    public List getCabeceras() {
+        return cabeceras;
+    }
+
+    public void setCabeceras(List cabeceras) {
+        this.cabeceras = cabeceras;
+    }
+
+    public Materias getMateriasSeleccionada() {
+        return materiasSeleccionada;
+    }
+
+    public void setMateriasSeleccionada(Materias materiasSeleccionada) {
+        this.materiasSeleccionada = materiasSeleccionada;
     }
 }
---
+++ usol/src/java/utilerias/NotasIngresar.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilerias;
+
+/**
+ *
+ * @author Geovanny
+ */
+public class NotasIngresar {
+    public Integer matricula;
+    public Double nota;
+    public Double desde;
+    public Double hasta;
+    public Boolean estado;
+    public String nombre;
+    public Boolean texto;
+    public String color;
+    public String colorEstado;
+
+    public NotasIngresar() {
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public Double getDesde() {
+        return desde;
+    }
+
+    public void setDesde(Double desde) {
+        this.desde = desde;
+    }
+
+    public Double getHasta() {
+        return hasta;
+    }
+
+    public void setHasta(Double hasta) {
+        this.hasta = hasta;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getTexto() {
+        return texto;
+    }
+
+    public void setTexto(Boolean texto) {
+        this.texto = texto;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColorEstado() {
+        return colorEstado;
+    }
+
+    public void setColorEstado(String colorEstado) {
+        this.colorEstado = colorEstado;
+    }
+    
+    
+    
+}
