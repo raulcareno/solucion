9e7a748648f74d281c78df9454b9a1062f76d5a6
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2013-05-20 02:17:29 +0000

    cst

--- usol/src/java/bean/PeriodosBean.java
+++ usol/src/java/bean/PeriodosBean.java
@@ -17,7 +17,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import jcinform.persistencia.Periodos;
 import jcinform.procesos.Administrador;
- 
+
 import utilerias.Permisos;
 
 /**
@@ -36,7 +36,7 @@ public class PeriodosBean {
     protected List<Periodos> model;
     public String textoBuscar;
     Permisos permisos;
-   Auditar  aud = new Auditar();
+    Auditar aud = new Auditar();
 
     public PeriodosBean() {
         //super();
@@ -83,50 +83,59 @@ public class PeriodosBean {
      */
     public String guardar() {
         FacesContext context = FacesContext.getCurrentInstance();
-if(object.getFechaInicio() == null){
-        FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ingrese la FECHA DE INICIO", ""));
-        return null;
-}
-if(object.getFechaFin() == null){
-        FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ingrese la FECHA FINAL ", ""));
-        return null;
-}
-        System.out.println("fecha ini: "+object.getFechaInicio().getTime());
-        System.out.println("fecha fin: "+object.getFechaFin().getTime());
-if(object.getFechaInicio().getTime() >= object.getFechaFin().getTime()){
-        FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "La FECHA DE INICIO no puede ser igual(=) o menor(<) a la FECHA FINAL", ""));
-        return null;
-}
+        if (object.getFechaInicio() == null) {
+            FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ingrese la FECHA DE INICIO", ""));
+            return null;
+        }
+        if (object.getFechaFin() == null) {
+            FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ingrese la FECHA FINAL ", ""));
+            return null;
+        }
+        System.out.println("fecha ini: " + object.getFechaInicio().getTime());
+        System.out.println("fecha fin: " + object.getFechaFin().getTime());
+        if (object.getFechaInicio().getTime() >= object.getFechaFin().getTime()) {
+            FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "La FECHA DE INICIO no puede ser igual(=) o menor(<) a la FECHA FINAL", ""));
+            return null;
+        }
         if (object.getIdPeriodos() == 0) {
             if (!permisos.verificarPermisoReporte("Periodos", "agregar_periodos.jspx", "agregar", true, "PARAMETROS")) {
-                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));                return null;
+                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));
+                return null;
             }
             try {
-                if (adm.existe("Periodos", "fechaInicio", object.getFechaInicio(), "fechaInicio", object.getFechaFin(),"").size() <= 0) {
+                if (adm.existe("Periodos", "fechaInicio", object.getFechaInicio(), "fechaInicio", object.getFechaFin(), "").size() <= 0) {
                     object.setIdPeriodos(adm.getNuevaClave("Periodos", "idPeriodos"));
                     adm.guardar(object);
-                    if(object.getActivo()){
-                        estadoPeriodos(object.getIdPeriodos()); 
+                    if (object.getActivo()) {
+                        estadoPeriodos(object.getIdPeriodos());
                     }
-                    aud.auditar(adm,this.getClass().getSimpleName().replace("Bean", ""), "guardar", "", object.getIdPeriodos()+"");
+                    if (object.getProximo()) {
+                        estadoPeriodosProximo(object.getIdPeriodos());
+                    }
+                    aud.auditar(adm, this.getClass().getSimpleName().replace("Bean", ""), "guardar", "", object.getIdPeriodos() + "");
                     inicializar();
                     FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage("Guardado...!"));
                 } else {
-                    FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR,"Periodo ya existe...!","Periodo ya existe...!"));
+                    FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "Periodo ya existe...!", "Periodo ya existe...!"));
                 }
             } catch (Exception e) {
                 FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
             }
         } else {
             if (!permisos.verificarPermisoReporte("Periodos", "actualizar_periodos.jspx", "agregar", true, "PARAMETROS")) {
-                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));                return null;
+                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));
+                return null;
             }
             try {
                 adm.actualizar(object);
-                aud.auditar(adm,this.getClass().getSimpleName().replace("Bean", ""), "actualizar", "", object.getIdPeriodos()+"");
-                if(object.getActivo()){
-                        estadoPeriodos(object.getIdPeriodos()); 
-                    }
+                aud.auditar(adm, this.getClass().getSimpleName().replace("Bean", ""), "actualizar", "", object.getIdPeriodos() + "");
+                if (object.getActivo()) {
+                    estadoPeriodos(object.getIdPeriodos());
+                }
+                if (object.getProximo()) {
+                        estadoPeriodosProximo(object.getIdPeriodos());
+                }
+                
                 FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage("Actualizado Correctamente...!"));
                 inicializar();
             } catch (Exception e) {
@@ -139,15 +148,23 @@ if(object.getFechaInicio().getTime() >= object.getFechaFin().getTime()){
 
         return null;
     }
-public void estadoPeriodos(Integer periodoActivo){
-    try {
-    adm.ejecutaSql("Update Periodos set activo = 0 where idPeriodos != '"+periodoActivo+"' ");    
-    } catch (Exception e) {
-        java.util.logging.Logger.getLogger(PeriodosBean.class.getName()).log(Level.SEVERE, null, e);
+
+    public void estadoPeriodos(Integer periodoActivo) {
+        try {
+            adm.ejecutaSql("Update Periodos set activo = 0 where idPeriodos != '" + periodoActivo + "' ");
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(PeriodosBean.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
     
-    
-}
+    public void estadoPeriodosProximo(Integer periodoActivo) {
+        try {
+            adm.ejecutaSql("Update Periodos set proximo = 0 where idPeriodos != '" + periodoActivo + "' ");
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(PeriodosBean.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
     /**
      * Elimina un registro asociado a la página
      */
@@ -155,10 +172,11 @@ public void estadoPeriodos(Integer periodoActivo){
         FacesContext context = FacesContext.getCurrentInstance();
         try {
             if (!permisos.verificarPermisoReporte("Periodos", "eliminar_periodos.jspx", "eliminar", true, "PARAMETROS")) {
-                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));                return null;
+                FacesContext.getCurrentInstance().addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage(FacesMessage.SEVERITY_ERROR, "No tiene permisos para realizar ésta acción", "No tiene permisos para realizar ésta acción"));
+                return null;
             }
             adm.eliminarObjeto(Periodos.class, obj.getIdPeriodos());
-            aud.auditar(adm,this.getClass().getSimpleName().replace("Bean", ""), "eliminar", "", obj.getIdPeriodos()+"");
+            aud.auditar(adm, this.getClass().getSimpleName().replace("Bean", ""), "eliminar", "", obj.getIdPeriodos() + "");
             inicializar();
             cargarDataModel();
             context.addMessage(findComponent(context.getViewRoot(), "form").getClientId(), new FacesMessage("Eliminado...!"));
@@ -179,7 +197,7 @@ public void estadoPeriodos(Integer periodoActivo){
             List<Periodos> datos = adm.query("Select o from Periodos as o");
             List<SelectItem> items = new ArrayList<SelectItem>();
             for (Periodos obj : datos) {
-                items.add(new SelectItem(obj, obj.getFechaInicio()+"-"+obj.getFechaFin()));
+                items.add(new SelectItem(obj, obj.getFechaInicio() + "-" + obj.getFechaFin()));
             }
             return items;
         } catch (Exception e) {
