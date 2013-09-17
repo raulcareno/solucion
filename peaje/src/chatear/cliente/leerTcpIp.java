b46a7044f1708d97bd11b3d2d93680e9335a0bda
jcinform@gmail.com <jcinform@gmail.com@7aa694e0-94bd-11de-8746-af773c80d763>
2013-09-17 21:36:26 +0000

    cst

--- peaje/src/chatear/cliente/leerTcpIp.java
+++ peaje/src/chatear/cliente/leerTcpIp.java
@@ -2,20 +2,35 @@ package chatear.cliente;
 
 import java.net.*;
 import java.io.*;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import peaje.formas.frmPrincipal;
+import peaje.formas.logger;
 
 public class leerTcpIp {
-public leerTcpIp(){
-    
-}
-    private frmPrincipal principal;
 
-    public void leerDatos(frmPrincipal pantalla,String ip, Integer puerto) {
+    public leerTcpIp() {
+    }
+    private frmPrincipal principal;
+    logger lger = new logger();
+
+    public void leerDatos(frmPrincipal pantalla, String ip, Integer puerto) {
         principal = pantalla;
         try {
+            
+                 Thread cargar = new Thread(ip) {
+                public void run() {
+                        ping(this.getName()); 
+                }
+            };
+            cargar.start();
+            
+        } catch (Exception e) {
+        }
+        try {
             int c;
+            Date fechaInicial = new Date();
             Socket s = null;
             InputStream sIn;
             // Abrimos una conexión con breogan en el puerto 4321
@@ -37,7 +52,7 @@ public leerTcpIp(){
                     datos = datos.replace(" ", "");
                     datos = datos.trim();
                     if (datos.length() == 8) {
-                        abrirbarrera(datos,ip);
+                        abrirbarrera(datos, ip);
                         datos = "";
                     }
                 } catch (InterruptedException ex) {
@@ -48,12 +63,62 @@ public leerTcpIp(){
             // Cuando se alcance el fin de fichero, cerramos la conexión y
             // abandonamos
             s.close();
+            lger.logger("leerTcpIP", "xxx CERRO CONEXION TCPIP \n "+"ABIERTA A LAS: "+fechaInicial.toLocaleString()+" CERRADO A LAS "+ (new Date()).toLocaleString());
+            try {
+                //VUELVO A RECONECTAR
+                System.out.println("VUELVO A RECONECTAR POR DESCONEXION O FALTA DE LECTURA EN EL SOCKET.....");
+                leerDatos(pantalla, ip, puerto);
+            } catch (Exception e) {
+                lger.logger("leerTcpIP", "ERROR AL RECONECTAR TCPIP: \n " + e.getMessage());
+                System.out.println("error al volver a reconectar");
+            }
+
         } catch (IOException ex) {
+            lger.logger("leerTcpIP", "PRUEBA"+ex.getMessage());
             Logger.getLogger(leerTcpIp.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    void abrirbarrera(String tarjeta,String ip) {
+    void ping(String ip) {
+        try {
+            InetAddress ping;
+            //String ip = "192.168.10.1"; // Ip de la máquina remota 
+            int sum = 0;
+            ping = InetAddress.getByName(ip);
+            while (sum >= 0) {
+
+
+                if (ping.isReachable(5000)) {
+                    System.out.println(ip + " - responde..!");
+                } else {
+                    lger.logger("leerTcpIP", "ERROR EN PING A: "+ip);
+                    System.out.println(ip + " - error de conexion con ip "+ip);
+                }
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(leerTcpIp.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                sum += 1000;
+                if (sum == 5000) {
+                    try {
+                        System.gc();
+                        System.out.println("");
+                        Thread.sleep(5000);
+                        sum = 0;
+                        System.gc();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(leerTcpIp.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                System.gc();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    void abrirbarrera(String tarjeta, String ip) {
         for (int i = tarjeta.length(); i < 10; i++) {
             tarjeta = "0" + tarjeta;
         }
